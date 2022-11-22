f8a17994a00e027e366072c65ddd6a6a4b65c7d3
zenby <zenbysel@gmail.com>
2022-11-22 13:18:31 +0200

    chore: mock environment for storybook

---
+++ apps/naris/.storybook/__mocks__/environment.ts
@@ -0,0 +1,14 @@
+import { EnvironmentInterface } from '../../src/environments/environment.interface';
+
+const HOST = '';
+export const environment: EnvironmentInterface = {
+  production: false,
+  host: HOST,
+  googleAuthUrl: HOST + '/api/auth/google',
+  patreonAuthUrl: HOST + '/api/auth/patreon',
+  yandexAuthUrl: HOST + '/api/auth/yandex',
+  apiUrl: HOST + '/api/',
+  assetsUrl: HOST + '/assets/',
+  privateAssetsUrl: HOST + '/assets/private/',
+  payServiceUrl: HOST + '/api/v2/seller',
+};

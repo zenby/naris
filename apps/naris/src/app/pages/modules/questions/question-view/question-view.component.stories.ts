486ca5e13afa7582b5759e80fde2c255c2e63ade
zenby <zenbysel@gmail.com>
2022-11-23 14:56:35 +0300

    refactor(question-view-story): move mocks to const

--- apps/naris/src/app/pages/modules/questions/question-view/question-view.component.stories.ts
+++ apps/naris/src/app/pages/modules/questions/question-view/question-view.component.stories.ts
@@ -7,42 +7,45 @@ import { Meta, moduleMetadata, Story } from '@storybook/angular';
 import { QuestionsConfigService } from '../services/questions-config.service';
 import { QuestionViewComponent } from './question-view.component';
 
+const activatedRouteMock =  {
+  snapshot: {
+    data: { question: null },
+  },
+}
+
+const dataStoreServiceMock = {
+  of: () =>
+    of({
+      payload: {
+        items: [
+          {
+            id: 2,
+            question:
+              'Здравствуйте SOER, хотел бы попросить у вас ссылку на космический трек для тестирования аудиокомпонента, спасибо!',
+            url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+            createdAt: '2021-09-05T19:53:18.000Z',
+            updatedAt: '2021-09-05T19:53:18.000Z',
+          },
+        ],
+      },
+    }),
+}
+
 export default {
   title: 'QuestionViewComponent',
   component: QuestionViewComponent,
   decorators: [
     moduleMetadata({
-      declarations: [],
       imports: [HttpClientModule, AudioPlayerModule],
       providers: [
+        QuestionsConfigService,
         {
           provide: ActivatedRoute,
-          useValue: {
-            snapshot: {
-              data: { question: null },
-            },
-          },
+          useValue: activatedRouteMock,
         },
-        { provide: QuestionsConfigService },
         {
           provide: DataStoreService,
-          useValue: {
-            of: () =>
-              of({
-                payload: {
-                  items: [
-                    {
-                      id: 2,
-                      question:
-                        'Здравствуйте SOER, хотел бы попросить у вас ссылку на космический трек для тестирования аудиокомпонента, спасибо!',
-                      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
-                      createdAt: '2021-09-05T19:53:18.000Z',
-                      updatedAt: '2021-09-05T19:53:18.000Z',
-                    },
-                  ],
-                },
-              }),
-          },
+          useValue: dataStoreServiceMock
         },
       ],
     }),

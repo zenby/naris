4f830bfbd6c5ac6cd70504ef4a882a3d41f5854d
nukink <zenbysel@gmail.com>
2022-12-01 10:45:18 +0200

    fix: remove unnecessary api prefixes for backend apps

--- apps/payment/src/main.ts
+++ apps/payment/src/main.ts
@@ -4,17 +4,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { setupSwagger } from './swagger';
 
-const API_PREFIX = 'api';
 const PORT = process.env.AUTH_PORT || '3300';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix(API_PREFIX);
 
   setupSwagger(app, PORT);
 
   await app.listen(PORT);
-  Logger.log(`🚀 Application is running on: http://localhost:${PORT}/${API_PREFIX}`);
+  Logger.log(`🚀 Application is running on: http://localhost:${PORT}`);
 }
 
 bootstrap();

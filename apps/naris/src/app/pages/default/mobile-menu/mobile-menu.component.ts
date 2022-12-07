324d092c45d2c98544e7fe9b40f20893d86794db
Nikolay_Tyumen <nikbabinov@mail.ru>
2022-12-08 00:26:25 +0500

    refactor(mobile menu):add menu animation and replace shadow menu to light-gray

--- apps/naris/src/app/pages/default/mobile-menu/mobile-menu.component.ts
+++ apps/naris/src/app/pages/default/mobile-menu/mobile-menu.component.ts
@@ -9,14 +9,33 @@ import { IMenuControl, MenuTree } from '../../../services/menu/menu.interfaces';
 })
 export class MobileMenuComponent {
   @Input() isMobile = false;
-  @Input() isMenuSohw = false;
   @Input() userInfo: UserModel = { id: -1, role: '', email: '' };
   @Input() applicationMenu: MenuTree = [];
   @Input() controls: IMenuControl[] | null = null;
   @Output() logout = new EventEmitter<boolean>();
   @Output() check = new EventEmitter<any>();
 
-  onShowMenu():void{
-    this.isMenuSohw = !this.isMenuSohw
+  classAnimationSCSS = 'displayNone';
+
+  onShowMenu(): void {
+    switch (this.classAnimationSCSS) {
+      case 'displayNone':
+        this.classAnimationSCSS = 'isShowMenu';
+        break;
+      case 'isShowMenu':
+        this.classAnimationSCSS = 'isHideMenu';
+        break;
+      case 'isHideMenu':
+        this.classAnimationSCSS = 'isShowMenu';
+        break;
+      default:
+        this.classAnimationSCSS = 'displayNone';
+    }
+  }
+
+  onHideMenu(): void {
+    if (this.classAnimationSCSS === 'isShowMenu'){
+      this.classAnimationSCSS = 'isHideMenu'
+    }
   }
 }

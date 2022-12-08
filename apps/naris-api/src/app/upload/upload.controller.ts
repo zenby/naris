e7774a07c8a866be3d8b5045730deb5748abc0b4
Alexey Kobevko <folco_2000@mail.ru>
2022-12-08 11:16:27 +0300

    docs: has been added swagger decorators

--- apps/naris-api/src/app/upload/upload.controller.ts
+++ apps/naris-api/src/app/upload/upload.controller.ts
@@ -5,12 +5,44 @@ import { join } from 'path';
 import { getFilenameHelper } from './helpers/get-filename.helper';
 import { UploadService } from './upload.service';
 import { HttpJsonStatus } from '../common/types/http-json-result.interface';
+import { ApiBody, ApiConsumes, ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Upload file' })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiCreatedResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        status: {
+          type: 'string',
+          example: 'ok',
+        },
+        items: {
+          type: 'array',
+          items: {
+            type: 'string',
+            example: '1670443088998-795980499.png',
+          },
+        },
+      },
+    },
+  })
   @UseInterceptors(
     FileInterceptor('file', {
       storage: diskStorage({

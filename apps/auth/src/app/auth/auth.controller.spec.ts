8e9d1dd451717184014b544b44be351122d6e5e8
Alexey Kobevko <folco_2000@mail.ru>
2022-12-14 23:59:59 +0300

    test: try testing controller

---
+++ apps/auth/src/app/auth/auth.controller.spec.ts
@@ -0,0 +1,43 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { SignInUserDto } from '../user/dto/sign-in-user.dto';
+
+const createUserDto: CreateUserDto = {
+  login: 'test login',
+  password: 'test password',
+  email: 'test@email.com',
+};
+
+describe('AuthController', () => {
+  let authController: AuthController;
+  let authService: AuthService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [
+        AuthService,
+        {
+          provide: AuthService,
+          useValue: {
+            getAccessToken: jest.fn().mockImplementation((user) => Promise.resolve('test-access-token')),
+            getRefreshToken: jest.fn().mockImplementation((user) => Promise.resolve('test-refresh-token')),
+            createUser: jest.fn().mockImplementation((createUserDto: CreateUserDto) => ({ id: 1, ...createUserDto })),
+            getComparedUser: jest
+              .fn()
+              .mockImplementation((signInUserDto: SignInUserDto) => ({ id: 1, ...signInUserDto })),
+          },
+        },
+      ],
+    }).compile();
+
+    authController = app.get<AuthController>(AuthController);
+    authService = app.get<AuthService>(AuthService);
+  });
+
+  it('should be defined', () => {
+    expect(authController).toBeDefined();
+  });
+});

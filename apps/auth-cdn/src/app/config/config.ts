f9678c779361dc90f4ad1a19ec30fe48bf644cb7
Alexey Kobevko <folco_2000@mail.ru>
2022-12-15 11:04:33 +0300

    refactor: remove unused variable from config

--- apps/auth-cdn/src/app/config/config.ts
+++ apps/auth-cdn/src/app/config/config.ts
@@ -3,7 +3,6 @@ import { join } from 'path';
 export interface Configuration {
   port: number;
   fileStoragePath: string;
-  verifyUrl: string;
   jwt: {
     jwtSecret: string;
   };
@@ -13,7 +12,6 @@ export function configurationFactory(): Configuration {
   return {
     port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3400,
     fileStoragePath: process.env.FILE_STORAGE_PATH ?? join(__dirname, '../../../', `apps/auth-cdn/src/assets`),
-    verifyUrl: process.env.VERIFY_URL,
     jwt: {
       jwtSecret: process.env.JWT_SECRET || 'Some random key here',
     },

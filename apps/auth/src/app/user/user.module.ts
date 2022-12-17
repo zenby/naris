9aa5a3e7c4bdf8bfd381b50842101c8108bc043c
Alexey Kobevko <folco_2000@mail.ru>
2022-12-17 20:53:03 +0300

    feat: add user module

---
+++ apps/auth/src/app/user/user.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
+
+@Module({
+  providers: [UserService],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  exports: [UserService],
+})
+export class UserModule {}
---
+++ apps/auth/src/app/user/user.service.ts
@@ -0,0 +1,51 @@
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { SignInUserDto } from './dto/sign-in-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const userByEmail = await this.userRepository.findOne({ where: { email: createUserDto.email } });
+
+    if (userByEmail) {
+      throw new HttpException('Email has already been taken', HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    const userByLogin = await this.userRepository.findOne({ where: { login: createUserDto.login } });
+
+    if (userByLogin) {
+      throw new HttpException('Login has already been taken', HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    const newUser = new UserEntity();
+
+    Object.assign(newUser, createUserDto);
+
+    return await this.userRepository.save(newUser);
+  }
+
+  async findByLogin(signInUserDto: SignInUserDto): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({ where: { login: signInUserDto.login } });
+
+    if (!user) {
+      throw new NotFoundException(`User with login ${signInUserDto.login} not found`);
+    }
+
+    return user;
+  }
+
+  async findByIdAndEmail({ id, email }: { id: number; email: string }): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({ where: { id, email } });
+
+    if (!user) {
+      throw new NotFoundException(`User not found`);
+    }
+
+    return user;
+  }
+}

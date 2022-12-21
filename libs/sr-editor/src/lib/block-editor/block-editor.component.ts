b7fd885d9304e8942477b61f7b67d60f40a25936
zenby <zenbysel@gmail.com>
2022-12-21 19:42:01 +0200

    refactor(sr-editor): move block editor

--- libs/sr-editor/src/lib/block-editor/block-editor.component.ts
+++ libs/sr-editor/src/lib/block-editor/block-editor.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-import { DelimitEvent, TextBlock } from '../../interfaces/document.model';
+import { DelimitEvent, TextBlock } from '../interfaces/document.model';
 
 @Component({
   selector: 'soer-block-editor',
--- libs/sr-editor/src/lib/sr-editor.module.ts
+++ libs/sr-editor/src/lib/sr-editor.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditorComponent } from './editor/editor.component';
-import { BlockEditorComponent } from './blocks/block-editor/block-editor.component';
+import { BlockEditorComponent } from './block-editor/block-editor.component';
 import { MarkdownModule } from 'ngx-markdown';
 import { FormsModule } from '@angular/forms';
 import { TextareaAutoresizeDirective } from './textarea-autoresize.directive';

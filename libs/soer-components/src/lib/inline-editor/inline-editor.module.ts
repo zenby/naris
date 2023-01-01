508af01b54dd0074cb3cd165e102978f0b78e72b
zenby <zenbysel@gmail.com>
2023-01-01 15:46:21 +0200

    refactor: remove unnecessary folder

--- libs/soer-components/src/lib/inline-editor/inline-editor.module.ts
+++ libs/soer-components/src/lib/inline-editor/inline-editor.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { InlineEditorComponent } from './inline-editor/inline-editor.component';
+import { InlineEditorComponent } from './inline-editor.component';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
   declarations: [InlineEditorComponent],
-  imports: [
-    CommonModule,
-    NzIconModule,
-    NzButtonModule,
-  ],
-  exports: [InlineEditorComponent]
+  imports: [CommonModule, NzIconModule, NzButtonModule],
+  exports: [InlineEditorComponent],
 })
 export class InlineEditorModule {}

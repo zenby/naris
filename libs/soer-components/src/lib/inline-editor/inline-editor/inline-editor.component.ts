049dcf348925f144e6cc6583b2825dcb279c0955
zenby <zenbysel@gmail.com>
2023-01-01 15:36:12 +0200

    refactor: use library text-editor component in the project

--- libs/soer-components/src/lib/inline-editor/inline-editor/inline-editor.component.ts
+++ libs/soer-components/src/lib/inline-editor/inline-editor/inline-editor.component.ts
@@ -2,22 +2,12 @@ import { AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, View
 
 @Component({
   selector: 'soer-inline-text-edit',
-  template: `
-      <textarea #textarea
-          (blur)="confirm()"
-          (input)="onInput($event)"
-          (keydown.esc)="onCancel()"
-          (keydown.enter)="onEnter($event)"
-      ></textarea>
-      <button nz-button nzSize="large" nzType="link" (click)="onEnter($event)" [disabled]="currentText.length === 0"><i nz-icon nzType="check-circle"></i></button>
-      <button nz-button nzDanger nzSize="large" nzType="link" (click)="onCancel()"><i nz-icon nzType="close-circle"></i></button>
-  `,
-  styleUrls: ['./inline-editor.component.scss']
+  templateUrl: './inline-editor.component.html',
+  styleUrls: ['./inline-editor.component.scss'],
 })
 export class InlineEditorComponent implements AfterViewInit {
-
-  @Input() srText = '';
-  @Output() srTextChange = new EventEmitter<string>();
+  @Input() text = '';
+  @Output() textChange = new EventEmitter<string>();
   @Output() readonly ok = new EventEmitter<string>(true);
   @Output() readonly cancel = new EventEmitter<string>(true);
   beforeText = '';
@@ -25,12 +15,11 @@ export class InlineEditorComponent implements AfterViewInit {
   @ViewChild('textarea', { static: false }) textarea!: ElementRef<HTMLTextAreaElement>;
 
   ngAfterViewInit(): void {
-    this.beforeText = this.currentText =  this.srText;
+    this.beforeText = this.currentText = this.text;
     this.focusAndSet();
   }
 
   confirm(): void {
-    
     setTimeout(() => {
       if (this.currentText !== this.beforeText) {
         this.ok.emit(this.currentText);
@@ -38,13 +27,12 @@ export class InlineEditorComponent implements AfterViewInit {
         this.onCancel();
       }
     }, 100);
-
   }
 
   onInput(event: Event): void {
     const target = event.target as HTMLTextAreaElement;
     this.currentText = target.value;
-    this.srTextChange.emit(target.value);
+    this.textChange.emit(target.value);
   }
 
   onEnter(event: Event): void {
@@ -54,13 +42,13 @@ export class InlineEditorComponent implements AfterViewInit {
   }
 
   onCancel(): void {
-    this.srTextChange.emit(this.beforeText);
+    this.textChange.emit(this.beforeText);
     this.cancel.emit(this.currentText);
   }
 
   private focusAndSet(): void {
     if (this.textarea?.nativeElement) {
-      setTimeout( () => {
+      setTimeout(() => {
         this.textarea.nativeElement.value = this.currentText || '';
         this.textarea.nativeElement.focus();
       }, 0);

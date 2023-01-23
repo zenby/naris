a0a5e06be21759ce2eafb5cf7e500ce37264e3b7
zenby <zenbysel@gmail.com>
2023-01-23 21:50:33 +0200

    refactor(editor): move code runner module to code block module

--- libs/sr-editor-blocks/src/lib/code-block/code-block.module.ts
+++ libs/sr-editor-blocks/src/lib/code-block/code-block.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CodeRunnerModule } from '@soer/sr-code-runner';
 import { CodeBlockComponent } from './code-block.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, CodeRunnerModule],
   declarations: [CodeBlockComponent],
   exports: [CodeBlockComponent],
 })
--- libs/sr-editor/src/lib/sr-editor.module.ts
+++ libs/sr-editor/src/lib/sr-editor.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CodeRunnerModule } from '@soer/sr-code-runner';
+
 import { MarkdownModule } from 'ngx-markdown';
 import { BlockEditorComponent } from './block-editor/block-editor.component';
 import { EditorComponent } from './editor/editor.component';
@@ -12,7 +12,7 @@ import { TextareaAutoresizeDirective } from './textarea-autoresize.directive';
 import { SrEditorBlocksModule } from '@soer/sr-editor-blocks';
 
 @NgModule({
-  imports: [CommonModule, MarkdownModule.forRoot(), FormsModule, CodeRunnerModule, SrEditorBlocksModule],
+  imports: [CommonModule, MarkdownModule.forRoot(), FormsModule, SrEditorBlocksModule],
   declarations: [
     BlockEditorComponent,
     BooleanToStringPipe,

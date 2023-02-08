aea3a2e6c16c606e3546cbe50887cb9d4968cc5a
Yaroslav Malykhin <yaroslav.malykhin@aimprosoft.com>
2023-02-08 16:41:02 +0200

    test: add unit-tests for pdf controller

--- apps/naris-api/src/app/modules/pdf/pdf.controller.spec.ts
+++ apps/naris-api/src/app/modules/pdf/pdf.controller.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
 import { PdfController } from './pdf.controller';
 import { PDFService } from './pdf.service';
 
 describe('PdfController', () => {
-  let controller: PdfController;
+  let pdfController: PdfController;
+  let pdfService: PDFService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,10 +14,39 @@ describe('PdfController', () => {
       providers: [PDFService],
     }).compile();
 
-    controller = module.get<PdfController>(PdfController);
+    pdfController = module.get<PdfController>(PdfController);
+    pdfService = module.get<PDFService>(PDFService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should be defined pfdController', () => {
+    expect(pdfController).toBeDefined();
+  });
+
+  it('should return exception', (done) => {
+    const requestMock = { content: 'pdf' };
+    const responseMock = {
+      status: jest.fn().mockImplementation().mockReturnValue(HttpStatus.OK),
+      write: jest.fn().mockImplementation(),
+      end: jest.fn((x) => x),
+    } as unknown as Response;
+
+    jest.spyOn(pdfService, 'convert').mockImplementation(() => Promise.reject('PDF exception'));
+    pdfController.getPdf(requestMock, responseMock).catch((error) => {
+      expect(error).toBeInstanceOf(HttpException);
+      done();
+    });
+  });
+
+  it('should return pdf success', async () => {
+    const mockPdf = Buffer.from([]);
+    const requestMock = { content: 'pdf' };
+    const responseMock = {
+      status: jest.fn().mockImplementation().mockReturnValue(HttpStatus.OK),
+      write: jest.fn().mockImplementation().mockReturnValue(mockPdf),
+      end: jest.fn((x) => x),
+    } as unknown as Response;
+
+    jest.spyOn(pdfService, 'convert').mockImplementation(async () => mockPdf);
+    expect(await pdfController.getPdf(requestMock, responseMock)).toEqual(responseMock.end());
   });
 });

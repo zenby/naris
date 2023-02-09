02121d1d4364130bbd180c911638fef83236a7aa
Menyalin Alexey <menyalin@gmail.com>
2023-02-09 08:23:31 +0300

    refactor: add pdf api to swagger docs

--- apps/naris-api/src/app/modules/pdf/pdf.controller.ts
+++ apps/naris-api/src/app/modules/pdf/pdf.controller.ts
@@ -1,22 +1,31 @@
 import { Body, Controller, Header, HttpCode, HttpException, HttpStatus, Post, Res } from '@nestjs/common';
-import {Response} from 'express'
-import { PDFService } from './pdf.service'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
+import { GetPdfDto } from './dto/getPdf.dto';
+import { PDFService } from './pdf.service';
+
+@ApiTags('MdToPdf')
 @Controller('document/convertor/MdToPdf')
 export class PdfController {
-    constructor(private readonly PDFService: PDFService){
+  constructor(private readonly PDFService: PDFService) {}
 
+  @Post()
+  @Header('Content-Type', 'application/pdf')
+  @Header('Content-Disposition', 'attachment')
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Convert MD string to PDF file', description: 'Convert markdown string to pdf' })
+  @ApiResponse({
+    status: 200,
+    description: 'Response contains the PDF file received from the passed content string.',
+    type: Buffer,
+  })
+  async getPdf(@Body() md: GetPdfDto, @Res() res: Response) {
+    try {
+      const pdfBuffer = await this.PDFService.convert(md.content);
+      res.write(pdfBuffer);
+      res.end();
+    } catch (e) {
+      throw new HttpException(e.message, HttpStatus.BAD_REQUEST);
     }
-    @Post() 
-    @Header('Content-Type', 'application/pdf')
-    @Header('Content-Disposition', 'attachment')
-    @HttpCode(200)
-    async getPdf(@Body() md: {content: string}, @Res() res: Response) {
-      try {
-        const pdfBuffer = await this.PDFService.convert(md.content);
-        res.write(pdfBuffer)
-        res.end()
-      } catch (e) {
-        throw new HttpException(e.message, HttpStatus.BAD_REQUEST)
-      }
-    }
+  }
 }

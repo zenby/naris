a453b49d9f3ed5f1dfd6dfc941c346733c2479e2
sk3ll <sk3llyar@gmail.com>
2023-02-09 20:52:39 +0200

    test: rewrite names of tests, added test for checking service, added supertest for checking method of request

--- apps/naris-api/src/app/modules/pdf/pdf.controller.spec.ts
+++ apps/naris-api/src/app/modules/pdf/pdf.controller.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { HttpException, HttpStatus } from '@nestjs/common';
+import { HttpException, HttpStatus, INestApplication } from '@nestjs/common';
 import { Response } from 'express';
+import * as request from 'supertest';
+import * as PdfConverter from 'md-to-pdf';
 import { PdfController } from './pdf.controller';
 import { PDFService } from './pdf.service';
 
 describe('PdfController', () => {
   let pdfController: PdfController;
   let pdfService: PDFService;
+  let app: INestApplication;
+
+  const requestMock = { content: 'pdfParams' };
+  const responseMock = {
+    write: jest.fn(),
+    end: jest.fn(),
+  } as unknown as Response;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,16 +25,11 @@ describe('PdfController', () => {
 
     pdfController = module.get<PdfController>(PdfController);
     pdfService = module.get<PDFService>(PDFService);
+    app = module.createNestApplication();
+    await app.init();
   });
 
-  it('should be defined pfdController', () => {
-    expect(pdfController).toBeDefined();
-  });
-
-  it('should return exception', (done) => {
-    const requestMock = { content: 'pdfParams' };
-    const responseMock = {} as unknown as Response;
-
+  it('should throw BadRequest exception on error', (done) => {
     jest.spyOn(pdfService, 'convert').mockImplementation(() => Promise.reject('PDF exception'));
     pdfController.getPdf(requestMock, responseMock).catch((error) => {
       expect(pdfService.convert).toBeCalledWith(requestMock.content);
@@ -35,18 +39,29 @@ describe('PdfController', () => {
     });
   });
 
-  it('should return pdf success', async () => {
-    const pdfMock = Buffer.from([]);
-    const requestMock = { content: 'pdfParams' };
-    const responseMock = {
-      write: jest.fn(),
-      end: jest.fn(),
-    } as unknown as Response;
+  it('should have pdfService for conversation md2pdf', async () => {
+    const md2PdfMock = jest.spyOn(PdfConverter, 'mdToPdf');
+    await pdfController.getPdf(requestMock, responseMock);
+    expect(md2PdfMock).toBeCalledWith(requestMock);
+  });
 
+  it('should contain public POST method method to convert getPdf', async () => {
+    const pdfMock = Buffer.from([]);
+    jest.spyOn(pdfService, 'convert').mockImplementation(async () => pdfMock);
+
+    await request(app.getHttpServer()).post('/document/convertor/MdToPdf').expect(HttpStatus.OK);
+  });
+
+  it('should use return result via (res: Response).write', async () => {
+    const pdfMock = Buffer.from([]);
     jest.spyOn(pdfService, 'convert').mockImplementation(async () => pdfMock);
     await pdfController.getPdf(requestMock, responseMock);
 
     expect(pdfService.convert).toBeCalledWith(requestMock.content);
     expect(responseMock.write).toBeCalledWith(pdfMock);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });

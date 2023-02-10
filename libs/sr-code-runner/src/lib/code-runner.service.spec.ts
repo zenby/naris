dc4a22a6979d9b9fdafb1e9eb3ae63fbf22cd106
Peter Chibisov <petr.v.chibisov@gmail.com>
2023-02-10 23:59:41 +0300

    test(sr-code-runner): improve test according to recomendations
    issue #105

--- libs/sr-code-runner/src/lib/code-runner.service.spec.ts
+++ libs/sr-code-runner/src/lib/code-runner.service.spec.ts
@@ -1,51 +1,50 @@
-import { Subscription } from 'rxjs';
-import { CodeLanguage } from './code-runner.model';
+import { of } from 'rxjs';
+import { CodeExecutionResult, CodeLanguage } from './code-runner.model';
 import { CodeRunnerService } from './code-runner.service';
 import { CodeExecutor } from './executors/code-executor.interface';
 
 describe('CodeRunnerService', () => {
   let service: CodeRunnerService;
 
-  const MockCodeExecutor = {
-    language: 'javascript',
-    runCode: jest.fn(),
-    terminate: jest.fn(),
-  } as unknown as CodeExecutor;
-
   const mockCode = 'const a = 123';
-  const subscriptions: Subscription[] = [];
+  const mockExecutionResult: CodeExecutionResult = {
+    level: 'result',
+    data: 'mock data',
+  };
+  const mockCodeExecutor: CodeExecutor = {
+    language: 'javascript',
+    runCode: jest.fn(() => of(mockExecutionResult)),
+    terminate: jest.fn(),
+  };
 
   beforeEach(() => {
-    service = new CodeRunnerService([MockCodeExecutor]);
+    service = new CodeRunnerService([mockCodeExecutor]);
   });
 
-  afterAll(() => {
-    subscriptions.forEach((sub) => sub.unsubscribe());
-  });
-
-  it('CodeRunnerService is defined', () => {
-    expect(service).toBeDefined();
-  });
-
-  it('`runCode` method is called', () => {
+  it('runCode method calls code executor with correct arguments', () => {
     service.runCode('javascript', mockCode);
-    expect(MockCodeExecutor.runCode).toBeCalled();
+    expect(mockCodeExecutor.runCode).toBeCalledWith(mockCode);
   });
 
-  it('`runCode` method returns correct CodeExecutionResult for wrong language', (done) => {
+  it('runCode method returns correct results for known language', (done) => {
+    service.runCode('javascript', mockCode).subscribe((result) => {
+      expect(result).toEqual(mockExecutionResult);
+      done();
+    });
+  });
+
+  it('runCode method returns correct results for unknown language', (done) => {
     const wrongLanguage = 'wrong language' as CodeLanguage;
-    subscriptions.push(
-      service.runCode(wrongLanguage, mockCode).subscribe((result) => {
-        expect(result.level).toEqual('error');
-        expect(result.data).toBeDefined();
-        done();
-      })
-    );
+    service.runCode(wrongLanguage, mockCode).subscribe((result) => {
+      expect(result.level).toEqual('error');
+      expect(result.data).toBeDefined();
+      done();
+    });
   });
 
-  it('`terminate` method terminates worker', () => {
+  it('terminate method terminates executor', () => {
     service.runCode('javascript', mockCode);
     service.terminate();
-    expect(MockCodeExecutor.terminate).toBeCalled();
+    expect(mockCodeExecutor.terminate).toBeCalled();
   });
 });

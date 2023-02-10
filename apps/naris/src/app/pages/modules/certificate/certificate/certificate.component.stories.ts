bbf3a3aefe3ac317fe01ed65fcd894cfff7ac124
xareyli <xareyli@gmail.com>
2023-02-10 16:43:17 +0300

    fix: fixed console errors by adding imports

--- apps/naris/src/app/pages/modules/certificate/certificate/certificate.component.stories.ts
+++ apps/naris/src/app/pages/modules/certificate/certificate/certificate.component.stories.ts
@@ -6,6 +6,9 @@ import { of } from 'rxjs';
 import { SrAuthModule } from '@soer/sr-auth';
 import { DataStoreService, SrDTOModule } from '@soer/sr-dto';
 import { ANY_SERVICE } from '@soer/mixed-bus';
+import { DemoNgZorroAntdModule } from '../../../demo.module';
+import { FormsModule } from '@angular/forms'; 
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 const mockDataStoreService = {
   of: (manifestId: any) => {
@@ -34,7 +37,7 @@ export default {
   component: CertificateComponent,
   decorators: [
     moduleMetadata({
-      imports: [RouterModule.forRoot([], { useHash: true }), HttpClientModule, SrAuthModule, SrDTOModule],
+      imports: [RouterModule.forRoot([], { useHash: true }), HttpClientModule, DemoNgZorroAntdModule, BrowserAnimationsModule, NoopAnimationsModule, FormsModule, SrAuthModule, SrDTOModule],
       providers: [
         { provide: 'manifest', useValue: ANY_SERVICE },
         { provide: DataStoreService, useValue: mockDataStoreService },

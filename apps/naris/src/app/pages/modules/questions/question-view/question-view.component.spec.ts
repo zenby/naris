21049fac3eda80466b36ebe41962c6ccd12d9bfa
zenby <zenbysel@gmail.com>
2023-02-11 15:42:52 +0200

    test(question-view): update test

--- apps/naris/src/app/pages/modules/questions/question-view/question-view.component.spec.ts
+++ apps/naris/src/app/pages/modules/questions/question-view/question-view.component.spec.ts
@@ -3,12 +3,18 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ANY_SERVICE } from '@soer/mixed-bus';
+import { QuestionsConfigService } from '../services/questions-config.service';
 
 import { QuestionViewComponent } from './question-view.component';
 
 describe('QuestionViewComponent', () => {
   let component: QuestionViewComponent;
   let fixture: ComponentFixture<QuestionViewComponent>;
+
+  const questionsConfigServiceMock = {
+    setAudioPlayerSpeed: jest.fn(),
+    getAudioPlayerSpeed: jest.fn(() => 5),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,6 +24,7 @@ describe('QuestionViewComponent', () => {
         { provide: ActivatedRoute, useValue: { snapshot: { data: { question: ANY_SERVICE } } } },
         { provide: 'question', useValue: ANY_SERVICE },
         { provide: 'questionsAll', useValue: ANY_SERVICE },
+        { provide: QuestionsConfigService, useValue: questionsConfigServiceMock },
       ],
     }).compileComponents();
   });
@@ -28,7 +35,14 @@ describe('QuestionViewComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should set audioPlayer speed from questionsControlService', () => {
+    expect(component.audioSpeed).toEqual(5);
+    expect(questionsConfigServiceMock.getAudioPlayerSpeed).toHaveBeenCalled();
+  });
+
+  it('changeAudioSpeed method calls questionsControlService with correct arguments', () => {
+    component.changeAudioSpeed(2);
+
+    expect(questionsConfigServiceMock.setAudioPlayerSpeed).toHaveBeenCalledWith(2);
   });
 });

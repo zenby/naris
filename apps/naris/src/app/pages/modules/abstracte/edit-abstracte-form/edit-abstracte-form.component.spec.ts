6e12ca6692b8961ea208dc1e75764316c6cb7dc7
ZemEr <egor.zemtzovskij@yandex.ru>
2023-02-11 17:54:20 +0300

    test: add test when form is in preview state

--- apps/naris/src/app/pages/modules/abstracte/edit-abstracte-form/edit-abstracte-form.component.spec.ts
+++ apps/naris/src/app/pages/modules/abstracte/edit-abstracte-form/edit-abstracte-form.component.spec.ts
@@ -1,7 +1,9 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Component, Input, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { EMPTY_WORKBOOK, TextBlockType, WorkbookModel } from '@soer/sr-editor';
+import { faker } from '@faker-js/faker';
 
 import { EditAbstracteFormComponent } from './edit-abstracte-form.component';
 
@@ -11,9 +13,25 @@ describe('EditAbstracteFormComponent', () => {
   let router: Router;
   let route: ActivatedRoute;
 
+  function fakeWorkbook(): WorkbookModel {
+    return {
+      id: Number.parseInt(faker.random.numeric()),
+      question: faker.random.words(3),
+      blocks: [
+        { text: faker.random.words(2), type: 'markdown' as TextBlockType },
+        { text: faker.random.words(5), type: 'markdown' as TextBlockType },
+      ],
+    };
+  }
+
+  @Component({ selector: 'soer-editor', template: `` })
+  class SoerEditorStubComponent {
+    @Input() workbook: WorkbookModel = EMPTY_WORKBOOK;
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [EditAbstracteFormComponent],
+      declarations: [EditAbstracteFormComponent, SoerEditorStubComponent],
       imports: [RouterTestingModule.withRoutes([])],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
@@ -28,4 +46,30 @@ describe('EditAbstracteFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('form is in preview state', () => {
+    const workbook = fakeWorkbook();
+
+    beforeEach(async () => {
+      component.workbook = workbook;
+      component.previewFlag = true;
+      fixture.detectChanges();
+    });
+
+    it('should not be displayed soer-editor', async () => {
+      expect(fixture.nativeElement.querySelector('soer-editor')).toBeNull();
+    });
+
+    it('should be contained markdown blocks with workbook blocks text', async () => {
+      workbook.blocks.forEach((block) => {
+        expect(
+          fixture.debugElement.query(
+            (element) => element.name == 'markdown' && element.nativeElement.textContent == block.text
+          )
+        ).not.toBeNull();
+      });
+    });
+
+  });
+
 });

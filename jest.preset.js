fd47e2c375bae799eb504386a7c307a1ae5e510c
zenby <zenbysel@gmail.com>
2023-02-11 15:42:22 +0200

    chore: add sr-local-storage library to jest global config

--- jest.preset.js
+++ jest.preset.js
@@ -1,13 +1,14 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { 
-    ...nxPreset,
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
+module.exports = {
+  ...nxPreset,
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
     '@soer/sr-auth': '<rootDir>/../../libs/sr-auth/src/index.ts',
     '@soer/mixed-bus': '<rootDir>/../../libs/mixed-bus/src/index.ts',
     '@soer/sr-dto': '<rootDir>/../../libs/sr-dto/src/index.ts',
     '@soer/sr-url-builder': '<rootDir>/../../libs/sr-url-builder/src/index.ts',
-    '@soer/sr-editor': '<rootDir>/../../libs/sr-editor/src/index.ts'
-  }
+    '@soer/sr-editor': '<rootDir>/../../libs/sr-editor/src/index.ts',
+    '@soer/sr-local-storage': '<rootDir>/../../libs/sr-local-storage/src/index.ts',
+  },
 };

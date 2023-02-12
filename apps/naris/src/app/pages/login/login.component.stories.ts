998d5bf296b4b9c53ec13d9fe436d8b58fc633d5
Никита <nikproserg@gmail.com>
2023-02-12 18:01:33 +0800

    chore: add stories to login component

---
+++ apps/naris/src/app/pages/login/login.component.stories.ts
@@ -0,0 +1,28 @@
+import { LoginComponent } from './login.component';
+import { Meta, moduleMetadata, Story } from "@storybook/angular";
+import {RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {DemoNgZorroAntdModule} from "../demo.module";
+import {SrAuthModule} from '@soer/sr-auth';
+
+export default {
+  title: 'LoginComponent',
+  component: LoginComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [RouterModule.forRoot([], { useHash: true }), SrAuthModule, HttpClientModule, DemoNgZorroAntdModule],
+      providers: [
+        { provide: 'AuthServiceConfig', useValue: {} },
+      ]
+    })
+  ]
+} as Meta<LoginComponent>;
+
+const Template: Story<LoginComponent> = (args: LoginComponent) => ({
+  props: args,
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  loading: false,
+};

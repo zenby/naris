c429805194aeee72fb5d50870a5d74ce6f9d353d
Peter Chibisov <petr.v.chibisov@gmail.com>
2023-02-14 15:23:10 +0300

    test(naris): fix task-edit-form.component.spec.ts
    issue #152

--- apps/naris/src/app/pages/modules/targets/task-edit-form/task-edit-form.component.spec.ts
+++ apps/naris/src/app/pages/modules/targets/task-edit-form/task-edit-form.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ANY_SERVICE } from '@soer/mixed-bus';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { DumbModule } from '../../../dumb/dumb.module';
 
 import { TaskEditFormComponent } from './task-edit-form.component';
 
@@ -12,7 +14,7 @@ describe('TaskEditFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TaskEditFormComponent],
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, DumbModule, NzEmptyModule],
       providers: [
         { provide: ActivatedRoute, useValue: { snapshot: { data: { target: ANY_SERVICE } } } },
         { provide: 'target', useValue: ANY_SERVICE },

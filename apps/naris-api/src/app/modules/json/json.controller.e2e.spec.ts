80cd14bb381b72cb815b78d38d8090ee12a57a4d
Karim <karim.kimsanbaev@gmail.com>
2023-02-20 17:39:59 +0300

    test(json):  polish tests after review

--- apps/naris-api/src/app/modules/json/json.controller.e2e.spec.ts
+++ apps/naris-api/src/app/modules/json/json.controller.e2e.spec.ts
@@ -5,6 +5,7 @@ import * as supertest from 'supertest';
 import { Test } from '@nestjs/testing';
 import { HttpJsonStatus } from '../../common/types/http-json-response.interface';
 import { DataSource } from 'typeorm';
+import { faker } from '@faker-js/faker';
 
 const jsonRepositoryMock = {
   find: jest.fn(),
@@ -24,7 +25,7 @@ const dataSourceMockHack = {
   getRepository: () => jsonRepositoryMock,
 };
 
-describe('Auth e2e-test', () => {
+describe('JsonModule e2e-test', () => {
   let app: INestApplication;
   let request: ReturnType<typeof supertest>;
 
@@ -52,91 +53,93 @@ describe('Auth e2e-test', () => {
   });
 
   describe('GET /json/:documentGroup/', () => {
-    it('should find all documents when pass without concrete document id', () => {
-      const documentGroup = 'test-document-group';
-      const document = {};
+    it('should find all documents when pass without concrete document id', async () => {
+      const document = createFakeDocument();
 
-      jest
-        .spyOn(jsonRepositoryMock, 'find')
-        .mockImplementationOnce((options) => (options.where.group == documentGroup ? [document] : []));
+      jsonRepositoryMock.find.mockReturnValueOnce([document]);
 
-      return request.get(`/json/${documentGroup}`).expect({ status: HttpJsonStatus.Ok, items: [document] });
+      await request.get(`/json/${document.group}`).expect({ status: HttpJsonStatus.Ok, items: [document] });
+
+      expect(jsonRepositoryMock.find).toHaveBeenCalledWith({ where: { group: document.group } });
     });
   });
 
   describe('POST /json/:documentGroup/new', () => {
-    it('should create json when passing valid json dto', () => {
-      const jsonString =
-        '{"title":"Разобраться с документацией .http","overview":"Я впервые сталкиваюсь с подобным форматом и хотел бы его изучить. ","progress":0,"tasks":[]}';
-      const document = {};
+    it('should create json when passing valid json dto', async () => {
+      const document = createFakeDocument();
 
-      jest.spyOn(jsonRepositoryMock, 'save').mockImplementationOnce(() => document);
+      jsonRepositoryMock.save.mockReturnValueOnce(document);
 
-      return request
-        .post('/json/test-document-group/new')
-        .send({ jsonString })
+      await request
+        .post(`/json/${document.group}/new`)
+        .send({ json: document.json })
         .expect({ status: HttpJsonStatus.Ok, items: [document] });
+
+      expect(jsonRepositoryMock.save).toHaveBeenCalledWith({ group: document.group, json: document.json });
     });
   });
 
   describe('GET /json/:documentGroup/:documentId', () => {
-    it('should find document when the document is in the repository', () => {
-      const documentId = '123';
-      const documentGroup = 'test-document-group';
-      const document = {};
+    it('should find document when the document is in the repository', async () => {
+      const document = createFakeDocument();
 
-      jest.spyOn(jsonRepositoryMock, 'findOne').mockImplementationOnce((options) => {
-        if (options.where.id == documentId && options.where.group == documentGroup) return document;
-      });
+      jsonRepositoryMock.findOne.mockReturnValueOnce(document);
 
-      return request
-        .get(`/json/${documentGroup}/${documentId}`)
+      await request
+        .get(`/json/${document.group}/${document.id}`)
         .expect({ status: HttpJsonStatus.Ok, items: [document] });
+
+      expect(jsonRepositoryMock.findOne).toHaveBeenCalledWith({ where: { id: document.id, group: document.group } });
     });
   });
 
   describe('PUT /json/:documentGroup/:documentId', () => {
-    it('should update document when pass stored document id', () => {
-      const documentId = '123';
-      const documentGroup = 'test-document-group';
-      const jsonToUpdate = '{"title":"Это обновленный документ .http"}';
+    it('should update document when pass stored document id', async () => {
+      const documentId = faker.datatype.number();
+      const documentGroup = faker.lorem.word();
+      const jsonToUpdate = faker.lorem.text();
 
-      let storedDocument = { json: '{"title":"Это старый документ .http"}' };
-      jest.spyOn(jsonRepositoryMock, 'findOne').mockImplementationOnce((options) => {
-        if (options.where.id == documentId && options.where.group == documentGroup) return storedDocument;
-      });
+      let storedDocument = createFakeDocument();
+      storedDocument.id = documentId;
+      storedDocument.group = documentGroup;
 
-      jest.spyOn(jsonRepositoryMock, 'save').mockImplementationOnce((newDocument) => (storedDocument = newDocument));
+      // Воспроизводим ожидаемое поведение репозитория для добавления надежности тесту.
+      jsonRepositoryMock.findOne.mockReturnValueOnce(storedDocument);
+      jsonRepositoryMock.save.mockImplementationOnce((newDocument) => (storedDocument = newDocument));
 
-      return request
+      await request
         .put(`/json/${documentGroup}/${documentId}`)
         .send({ json: jsonToUpdate })
-        .expect({ status: HttpJsonStatus.Ok, items: [storedDocument] })
-        .then(() => {
-          expect(storedDocument.json).toBe(jsonToUpdate);
-        });
+        .expect({ status: HttpJsonStatus.Ok, items: [storedDocument] });
+
+      expect(storedDocument.json).toBe(jsonToUpdate);
+      expect(jsonRepositoryMock.findOne).toHaveBeenCalledWith({ where: { id: documentId, group: documentGroup } });
+      expect(jsonRepositoryMock.save).toHaveBeenCalledWith({ ...storedDocument, json: jsonToUpdate });
     });
   });
 
   describe('DELETE /json/:documentGroup/:documentId', () => {
-    it('should delete document when pass stored document id', () => {
-      const documentId = '123';
-      const documentGroup = 'test-document-group';
+    it('should delete document when pass stored document id', async () => {
+      const storedDocument = createFakeDocument();
 
-      let storedDocument = { json: '{"title":"Документ .http"}' };
-      jest.spyOn(jsonRepositoryMock, 'findOne').mockImplementationOnce((options) => {
-        if (options.where.id == documentId && options.where.group == documentGroup) return storedDocument;
-      });
-      jest.spyOn(jsonRepositoryMock, 'delete').mockImplementationOnce((options) => {
-        if (options.id == documentId) storedDocument = null;
-      });
+      jsonRepositoryMock.findOne.mockReturnValueOnce(storedDocument);
 
-      return request
-        .delete(`/json/${documentGroup}/${documentId}`)
-        .expect({ status: HttpJsonStatus.Ok, items: [] })
-        .then(() => {
-          expect(storedDocument).toBeNull();
-        });
+      await request
+        .delete(`/json/${storedDocument.group}/${storedDocument.id}`)
+        .expect({ status: HttpJsonStatus.Ok, items: [] });
+
+      expect(jsonRepositoryMock.findOne).toHaveBeenCalledWith({
+        where: { id: storedDocument.id, group: storedDocument.group },
+      });
+      expect(jsonRepositoryMock.delete).toHaveBeenCalledWith({ id: storedDocument.id });
     });
   });
 });
+
+function createFakeDocument() {
+  return {
+    id: faker.datatype.number(),
+    json: faker.lorem.text(),
+    group: faker.lorem.word(),
+  };
+}

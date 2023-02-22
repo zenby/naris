c791c03f421534bd5b54105d0b99be2510ec80c5
Menyalin Alexey <menyalin@gmail.com>
2023-02-22 11:35:30 +0300

    fix: refactor and fix remarks

--- apps/naris-api/src/app/modules/images/images.controller.ts
+++ apps/naris-api/src/app/modules/images/images.controller.ts
@@ -3,39 +3,30 @@ import { ApiBody, ApiConsumes, ApiOperation, ApiResponse, ApiTags } from '@nestj
 import { ImagesService } from './images.service';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { MAX_IMAGE_FILE_SIZE_MB} from './constants'
-
+import { saveImagesBodySchema } from './swagger.schemes'
 
 @ApiTags('Images')
 @Controller('images')
 export class ImagesController {
-  constructor(private readonly ImagesService: ImagesService) {}
+  constructor(private readonly imagesService: ImagesService) {}
 
   @Post()
   @UseInterceptors(FilesInterceptor('files'))
   @HttpCode(201)
   @ApiConsumes('multipart/form-data')
-  @ApiBody({schema: {
-    type: 'object',
-    properties: {
-        files: {
-          type: 'array',
-          format: 'binary',
-          description: 'Image files with mimetype ^image/*'
-        },
-      },
-  }})
+  @ApiBody(saveImagesBodySchema)
   @ApiOperation({ summary: 'Upload Images', description: `max file size: ${MAX_IMAGE_FILE_SIZE_MB}Mb. Allowed mimeTypes: 'image/*'` })
   @ApiResponse({
     status: 201,
     description: 'Response contain array of full paths uploaded images',
     type: Array<string>,
   })
-  async saveImages(
+  saveImages(
     @UploadedFiles()
     files: Express.Multer.File[]
   ) {
     try {
-      const paths: string[] = await this.ImagesService.getPathsToSavedImages(files);
+      const paths: string[] = this.imagesService.getPathsToSavedImages(files);
       return paths;
     } catch (e) {
       throw new HttpException(e.message, HttpStatus.BAD_REQUEST);
--- apps/naris-api/src/app/modules/images/images.module.ts
+++ apps/naris-api/src/app/modules/images/images.module.ts
@@ -2,38 +2,7 @@ import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 import { ImagesController } from './images.controller';
 import { ImagesService } from './images.service';
-import { diskStorage } from 'multer';
-import { v4 as uuidv4 } from 'uuid';
-import * as Path from 'node:path';
-import * as fs from 'node:fs/promises';
-import { MAX_IMAGE_FILE_SIZE_MB } from './constants';
-
-export function imageStorageBuilder(path: string, maxFileSizeMB = MAX_IMAGE_FILE_SIZE_MB) {
-  const limits = {
-    fileSize: maxFileSizeMB * 1024 * 1024,
-  };
-
-  function fileFilter(_req, file, cb) {
-    const regex = new RegExp('^image/', 'i');
-    if (regex.test(file.mimetype)) cb(null, true);
-    else cb(null, false);
-  }
-
-  const storage = diskStorage({
-    destination: async function (_req, _file, cb) {
-      try {
-        await fs.readdir(path);
-      } catch (e) {
-        if (e.code === 'ENOENT') await fs.mkdir(path);
-      }
-      cb(null, path);
-    },
-    filename: function (_req, file, cb) {
-      cb(null, `${uuidv4()}${Path.extname(file.originalname)}`);
-    },
-  });
-  return { storage, limits, fileFilter };
-}
+import { imageStorageBuilder } from './imageStorageBuilder';
 
 @Module({
   imports: [MulterModule.register(imageStorageBuilder('./images'))],

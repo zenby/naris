47a240f7edda85064c79b8d35429b93d9639e29c
Menyalin Alexey <menyalin@gmail.com>
2023-02-22 13:16:21 +0300

    fix remarks

--- apps/naris-api/src/app/modules/images/images.service.ts
+++ apps/naris-api/src/app/modules/images/images.service.ts
@@ -1,9 +1,9 @@
-import * as Path from 'node:path';
+import * as path from 'node:path';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ImagesService {
   getPathsToSavedImages(files: Express.Multer.File[]): string[] {
-    return files.map((f) => Path.normalize(f.path));
+    return files.map((f) => path.normalize(f.path));
   }
 }

ada425399a22353d16fa2c3e20cd0f3e12a08502
nukink <zenbysel@gmail.com>
2023-03-04 15:07:55 +0200

    test: fix editor blocks module tests

--- libs/sr-editor-blocks/src/lib/test-block/test-block.component.spec.ts
+++ libs/sr-editor-blocks/src/lib/test-block/test-block.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MarkdownModule } from 'ngx-markdown';
 import { TestBlockComponent } from './test-block.component';
 
 describe('TestBlockComponent', () => {
@@ -9,6 +9,7 @@ describe('TestBlockComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TestBlockComponent],
+      imports: [MarkdownModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TestBlockComponent);

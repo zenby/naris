bcfd49ad62eb1bdc30792f9fda832a9d221bbcdc
Mamin Paren <to.mamin.paren@gmail.com>
2023-03-05 22:48:19 +0800

    fix(auth): exclude sessions from yandex auth

--- apps/auth/src/app/common/guards/yandex-auth.guard.ts
+++ apps/auth/src/app/common/guards/yandex-auth.guard.ts
@@ -5,8 +5,6 @@ import { AuthGuard } from "@nestjs/passport";
 export class YandexAuthGuard extends AuthGuard('yandex') {
     async canActivate(context: ExecutionContext) {
         const activate = (await super.canActivate(context)) as boolean;
-        const request = context.switchToHttp().getRequest();
-        await super.logIn(request);
         return activate;  
     }
 }

884e106b4ad7cc7994609eaa0c88efb2b144a581
Karim Kimsanbaev <karim.kimsanbaev@gmail.com>
2023-03-05 14:55:28 +0300

    feat(google): add google auth strategy
    feat(google): add google auth strategy #2

---
+++ apps/auth/src/app/common/strategies/google.strategy.ts
@@ -0,0 +1,43 @@
+﻿import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { config } from 'dotenv';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserService } from '../../user/user.service';
+import { UserEntity } from '../../user/user.entity';
+import { CreateUserDto } from '../../user/dto/create-user.dto';
+import { Profile } from 'passport';
+
+config();
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(private readonly userService: UserService) {
+    super({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      scope: ['email'],
+    });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback): Promise<UserEntity | Error> {
+    const { emails } = profile;
+    const email = emails[0].value;
+
+    let user = await this.userService.findByEmail(email);
+    if (user instanceof NotFoundException) {
+      const createUser: CreateUserDto = {
+        login: email,
+        email: email,
+        password: '',
+      };
+
+      user = await this.userService.createOpenIdUser(createUser);
+    }
+
+    if (user instanceof Error) return user;
+
+    done(null, user);
+    return user;
+  }
+}
--- apps/auth/src/app/user/user.service.ts
+++ apps/auth/src/app/user/user.service.ts
@@ -3,9 +3,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-import { LoginUserDto } from './dto/login-user.dto';
 import { validate } from 'class-validator';
 import { ValidationErrorHelper } from '../common/helpers/validation-error.helper';
+import { CreateOpenIdUserDto } from './dto/create-open-id-user.dto';
 
 @Injectable()
 export class UserService {
@@ -27,11 +27,37 @@ export class UserService {
     });
   }
 
+  async createOpenIdUser(createUserDto: CreateOpenIdUserDto): Promise<UserEntity | Error> {
+    const newUser = new UserEntity();
+
+    Object.assign(newUser, createUserDto);
+
+    const errors = await validate(newUser);
+
+    if (errors?.length > 0) {
+      return new UnprocessableEntityException(ValidationErrorHelper.errorsToString(errors));
+    }
+
+    return await this.userRepository.save(newUser).catch(() => {
+      return new UnprocessableEntityException(`User with email ${newUser.email} has been already exist`);
+    });
+  }
+
   async findByLogin(login: string): Promise<UserEntity | Error> {
     const user = await this.userRepository.findOne({ where: { login } });
 
     if (!user) {
       return new NotFoundException(`User with login ${login} not found`);
+    }
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<UserEntity | Error> {
+    const user = await this.userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      return new NotFoundException(`User with email ${email} not found`);
     }
 
     return user;

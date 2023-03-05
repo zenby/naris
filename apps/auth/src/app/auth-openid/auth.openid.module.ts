189e8b6bf4170c242bb3644d8cd76ddb28ea9457
Karim Kimsanbaev <karim.kimsanbaev@gmail.com>
2023-03-05 14:56:08 +0300

    feat(auth-openid): setup auth open id module

---
+++ apps/auth/src/app/auth-openid/auth.openid.module.ts
@@ -0,0 +1,15 @@
+﻿import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule } from '@nestjs/config';
+import { GoogleStrategy } from '../common/strategies/google.strategy';
+import { AuthOpenIdController } from './auth.openid.controller';
+import { UserService } from '../user/user.service';
+import { AuthService } from '../auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Module({
+  imports: [PassportModule, ConfigModule.forRoot()],
+  controllers: [AuthOpenIdController],
+  providers: [GoogleStrategy, UserService, AuthService, JwtService],
+})
+export class AuthOpenIdModule {}

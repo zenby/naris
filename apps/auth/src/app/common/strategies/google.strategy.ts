90dd1746ed060a97c9f8937804bfacb5cff2fc4e
Karim Kimsanbaev <karim.kimsanbaev@gmail.com>
2023-03-06 23:09:50 +0300

    feat(auth-strategy): call auth open id service from google strategy

--- apps/auth/src/app/common/strategies/google.strategy.ts
+++ apps/auth/src/app/common/strategies/google.strategy.ts
@@ -1,17 +1,16 @@
 ﻿import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { config } from 'dotenv';
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { UserService } from '../../user/user.service';
+import { Injectable } from '@nestjs/common';
 import { UserEntity } from '../../user/user.entity';
-import { CreateUserDto } from '../../user/dto/create-user.dto';
 import { Profile } from 'passport';
+import { AuthOpenIdService } from '../../auth-openid/auth.openid.service';
 
 config();
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor(private readonly userService: UserService) {
+  constructor(private readonly authService: AuthOpenIdService) {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -20,22 +19,21 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     });
   }
 
-  async validate(accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback): Promise<UserEntity | Error> {
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: VerifyCallback
+  ): Promise<UserEntity | Error> {
     const { emails } = profile;
     const email = emails[0].value;
 
-    let user = await this.userService.findByEmail(email);
-    if (user instanceof NotFoundException) {
-      const createUser: CreateUserDto = {
-        login: email,
-        email: email,
-        password: '',
-      };
+    const user = await this.authService.authByOpenID(email);
 
-      user = await this.userService.createOpenIdUser(createUser);
+    if (user instanceof Error) {
+      done(user, undefined);
+      return user;
     }
-
-    if (user instanceof Error) return user;
 
     done(null, user);
     return user;

f1ed710b171856f802bfe18f0bf5fa7fcad24f05
Mamin Paren <to.mamin.paren@gmail.com>
2023-03-09 23:03:36 +0800

    feat(auth): implement handleRequest in YandexAuthGuard

--- apps/auth/src/app/common/guards/yandex-auth.guard.ts
+++ apps/auth/src/app/common/guards/yandex-auth.guard.ts
@@ -1,4 +1,4 @@
-import { ExecutionContext, Injectable } from "@nestjs/common";
+import { ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
@@ -6,5 +6,13 @@ export class YandexAuthGuard extends AuthGuard('yandex') {
     async canActivate(context: ExecutionContext) {
         const activate = (await super.canActivate(context)) as boolean;
         return activate;  
+    }
+
+    
+    handleRequest(err, user) {
+        if (err || !user) {
+            throw err || new ForbiddenException();
+        }
+        return user;
     }
 }

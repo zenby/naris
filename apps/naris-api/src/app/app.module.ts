226e22d3d307cafb7dd9ec02ff5cd940d00a50c9
leviskay <yupehov@ya.ru>
2023-03-11 12:45:16 +0300

    feat(naris-api): setup jwt functional

--- apps/naris-api/src/app/app.module.ts
+++ apps/naris-api/src/app/app.module.ts
@@ -1,21 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
 import { configurationFactory, typeOrmFactory } from './config/config';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JsonModule } from './modules/json/json.module';
 import { PdfModule } from './modules/pdf/pdf.module';
 import { ImagesModule } from './modules/images/images.module';
+import { JwtConfig } from './config/jwt.config';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule,
     ConfigModule.forRoot({
       envFilePath: '../../.env',
       isGlobal: true,
-      load: [configurationFactory],
+      load: [configurationFactory, JwtConfig],
     }),
     TypeOrmModule.forRootAsync({
       useFactory: typeOrmFactory,

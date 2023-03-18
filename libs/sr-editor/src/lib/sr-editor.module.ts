a92c4e41fe8525882565cb48828ba821a3a746d9
egor_wglzfg <actzch@gmail.com>
2023-03-18 17:37:39 +0300

    chore(block-editor-controls): change manual tooltip to nzTooltip

--- libs/sr-editor/src/lib/sr-editor.module.ts
+++ libs/sr-editor/src/lib/sr-editor.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { MarkdownModule } from 'ngx-markdown';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { BlockEditorComponent } from './components/block-editor/block-editor.component';
 import { BlockEditorControlsComponent } from './components/block-editor-controls/block-editor-controls.component';
 import { EditorComponent } from './components/editor/editor.component';
@@ -14,7 +15,7 @@ import { BlockService } from './services/block.service';
 import { TextareaAutoresizeDirective } from './textarea-autoresize.directive';
 
 @NgModule({
-  imports: [CommonModule, MarkdownModule.forRoot(), FormsModule, NzIconModule, NzButtonModule],
+  imports: [CommonModule, MarkdownModule.forRoot(), FormsModule, NzIconModule, NzButtonModule, NzToolTipModule],
   declarations: [
     BlockEditorComponent,
     BlockEditorControlsComponent,

f8da7eac7cd43d0e2f7d32156b76a1904cb01318
Mamin Paren <to.mamin.paren@gmail.com>
2023-03-22 22:45:15 +0800

    refactor(user-controller): add swagger decorators and validation pipe

--- apps/auth/src/app/user/user.controller.ts
+++ apps/auth/src/app/user/user.controller.ts
@@ -1,6 +1,26 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  InternalServerErrorException,
+  Logger,
+  Param,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
+import {
+  ApiFoundResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { HttpJsonResult, HttpJsonStatus } from '@soer/sr-common-interfaces';
+import { responseErrorSchema } from '../auth/doc/response-error.schema';
+import { responseSchema } from '../auth/doc/response.schema';
 import { Roles } from '../common/decorators/roles.decorator';
 import { RolesGuard } from '../common/guards/roles-guard';
+import { BackendValidationPipe } from '../common/pipes/backend-validation.pipe';
 import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
 
@@ -13,6 +33,14 @@ export class UserController {
   private internalErrorMessage = 'Something went wrong. Try it later';
 
   @Roles('admin')
+  @UsePipes(BackendValidationPipe)
+  @ApiOperation({
+    summary: 'Login',
+    description: "Requires body { login, password }. Returns HTTP_ONLY cookie['refresh_token']",
+  })
+  @ApiFoundResponse({ schema: responseSchema })
+  @ApiNotFoundResponse({ schema: responseSchema })
+  @ApiUnauthorizedResponse({ description: 'Should be an admin.' })
   @Get('users')
   async getUsers(): Promise<HttpJsonResult<UserEntity | string>> {
     try {
@@ -30,6 +58,10 @@ export class UserController {
   }
 
   @Roles('admin')
+  @UsePipes(BackendValidationPipe)
+  @ApiOkResponse({ schema: responseSchema })
+  @ApiNotFoundResponse({ schema: responseErrorSchema('User with id ${id} not found') })
+  @ApiUnauthorizedResponse({ description: 'Should be an admin.' })
   @Delete('user/:id')
   async deleteUser(@Param('id') id: number): Promise<HttpJsonResult<string>> {
     try {

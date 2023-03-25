2cf22c200c033f5431e3979aa88aca6ac3b7905b
Mamin Paren <to.mamin.paren@gmail.com>
2023-03-26 01:47:24 +0800

    refactor(user-controller-e2e): extract test users creation to stand-alone func

--- apps/auth/src/app/user/user.controller.e2e.spec.ts
+++ apps/auth/src/app/user/user.controller.e2e.spec.ts
@@ -10,6 +10,7 @@ import { Configuration } from '../config/config';
 import { UserEntity, UserRole } from './user.entity';
 import { UserModule } from './user.module';
 import { HttpJsonResult } from '@soer/sr-common-interfaces';
+import { faker } from '@faker-js/faker';
 
 describe('user controller e2e tests', () => {
   let app: INestApplication;
@@ -59,31 +60,8 @@ describe('user controller e2e tests', () => {
     let adminUser: UserEntity;
 
     beforeAll(async () => {
-      adminUser = new UserEntity();
-      Object.assign(adminUser, {
-        id: 1,
-        login: 'test',
-        email: 'test@test.example',
-        role: UserRole.ADMIN,
-      });
-
-      const user1 = new UserEntity();
-      Object.assign(user1, {
-        id: 2,
-        login: 'user1',
-        email: 'user2@test.example',
-        role: UserRole.USER,
-      });
-
-      const user2 = new UserEntity();
-      Object.assign(user2, {
-        id: 3,
-        login: 'user2',
-        email: 'user2@test.example',
-        role: UserRole.USER,
-      });
-
-      users = [user1, user2, adminUser];
+      adminUser = getTestUser({ role: UserRole.ADMIN });
+      users = [getTestUser(), getTestUser(), adminUser];
     });
 
     function isUserEntity(arg: object): arg is UserEntity[] {
@@ -106,20 +84,15 @@ describe('user controller e2e tests', () => {
         .set('Cookie', [`${config.cookieName}=${jwtToken}`])
         .send()
         .expect(200);
+
       const body: HttpJsonResult<UserEntity> = response.body;
 
-      expect(body.items.length == 3);
+      expect(body.items.length == users.length);
       expect(isUserEntity(body.items)).toBeTruthy;
     });
 
     it('should return 403 error when user not an admin', async () => {
-      const user = new UserEntity();
-      Object.assign(user, {
-        id: 2,
-        login: 'user1',
-        email: 'user2@test.example',
-        role: UserRole.USER,
-      });
+      const user = getTestUser();
 
       const { jwtSecret: secret, expiresInRefresh: expiresIn } = config;
 
@@ -145,21 +118,8 @@ describe('user controller e2e tests', () => {
 
   describe('DELETE /user/id', () => {
     it('should return ok status when user deleted by an admin', async () => {
-      const adminUser = new UserEntity();
-      Object.assign(adminUser, {
-        id: 1,
-        login: 'test',
-        email: 'test@test.example',
-        role: UserRole.ADMIN,
-      });
-
-      const userToDelete = new UserEntity();
-      Object.assign(userToDelete, {
-        id: 2,
-        login: 'user1',
-        email: 'user2@test.example',
-        role: UserRole.USER,
-      });
+      const adminUser = getTestUser({ role: UserRole.ADMIN });
+      const userToDelete = getTestUser();
 
       const { jwtSecret: secret, expiresInRefresh: expiresIn } = config;
 
@@ -176,6 +136,7 @@ describe('user controller e2e tests', () => {
         .set('Cookie', [`${config.cookieName}=${jwtToken}`])
         .send()
         .expect(200);
+
       const body: HttpJsonResult<DeleteResult> = response.body;
 
       expect(userRepositoryDeleteSpy).toHaveBeenCalledWith({ id: `${userToDelete.id}` });
@@ -183,21 +144,8 @@ describe('user controller e2e tests', () => {
     });
 
     it('should return 403 error when user not an admin', async () => {
-      const user = new UserEntity();
-      Object.assign(user, {
-        id: 2,
-        login: 'user1',
-        email: 'user2@test.example',
-        role: UserRole.USER,
-      });
-
-      const userToDelete = new UserEntity();
-      Object.assign(userToDelete, {
-        id: 2,
-        login: 'user1',
-        email: 'user2@test.example',
-        role: UserRole.USER,
-      });
+      const user = getTestUser();
+      const userToDelete = getTestUser();
 
       const { jwtSecret: secret, expiresInRefresh: expiresIn } = config;
 
@@ -216,15 +164,28 @@ describe('user controller e2e tests', () => {
     });
 
     it('should return 403 error when not authorized', async () => {
-      const userToDelete = new UserEntity();
-      Object.assign(userToDelete, {
-        id: 2,
-        login: 'user1',
-        email: 'user2@test.example',
-        role: UserRole.USER,
-      });
+      const userToDelete = getTestUser();
 
       await request.delete(`/user/${userToDelete.id}`).send().expect(403);
     });
   });
+
+  type UserInfo = {
+    id?: number;
+    login?: string;
+    email?: string;
+    role?: UserRole;
+  };
+
+  function getTestUser(userInfo: UserInfo = { id: undefined, login: undefined, email: undefined, role: undefined }) {
+    const user = new UserEntity();
+    Object.assign(user, {
+      id: userInfo.id ?? faker.random.numeric(),
+      login: userInfo.login ?? faker.internet.userName(),
+      email: userInfo.email ?? faker.internet.email(),
+      uuid: faker.datatype.uuid(),
+      role: userInfo.role ?? UserRole.USER,
+    });
+    return user;
+  }
 });

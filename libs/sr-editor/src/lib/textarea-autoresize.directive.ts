901487e45ee0a9ab5b830c12dd56ade3f4dcf55e
nukink <zenbysel@gmail.com>
2023-04-08 20:40:32 +0300

    feat: apply autoresize directive only for markdown blocks

--- libs/sr-editor/src/lib/textarea-autoresize.directive.ts
+++ libs/sr-editor/src/lib/textarea-autoresize.directive.ts
@@ -1,9 +1,10 @@
-import { Directive, ElementRef, HostListener, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[soerAutoresize]', // Attribute selector
 })
 export class TextareaAutoresizeDirective implements OnInit {
+  @Input('soerAutoresize') isEnabled = false;
   @HostListener('input', ['$event.target'])
   onInput(): void {
     this.adjust();
@@ -18,6 +19,7 @@ export class TextareaAutoresizeDirective implements OnInit {
   }
 
   adjust(): void {
+    if (!this.isEnabled) return;
     this.element.nativeElement.style.height = this.element.nativeElement.children[0].scrollHeight + 2 + 'px';
   }
 }

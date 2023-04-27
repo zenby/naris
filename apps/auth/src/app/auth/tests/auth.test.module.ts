881a6af32ffe4052dc131b37fb521ee6372e0128
Mamin Paren <to.mamin.paren@gmail.com>
2023-04-27 23:08:19 +0800

    refactor(auth-test-module): extract config mock to auth test module

--- apps/auth/src/app/auth/tests/auth.test.module.ts
+++ apps/auth/src/app/auth/tests/auth.test.module.ts
@@ -6,10 +6,37 @@ import { AuthService } from '../auth.service';
 import { LocalStrategy } from '../../common/strategies/local.strategy';
 import { RefreshCookieStrategy } from '../../common/strategies/refreshCookie.strategy';
 import { AuthController } from '../auth.controller';
+import { Configuration } from '../../config/config';
+import { ConfigService } from '@nestjs/config';
+import { UserEntity } from '../../user/user.entity';
+import * as jwt from 'jsonwebtoken';
+
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'cookie' }), JwtModule.register({}), UserTestModule],
-  providers: [AuthService, LocalStrategy, RefreshCookieStrategy],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    RefreshCookieStrategy,
+    {
+      provide: ConfigService,
+      useFactory: (_) => {
+        return configMock;
+      },
+    },
+  ],
   controllers: [AuthController],
   exports: [],
 })
 export class AuthTestModule {}
+
+export const config: Configuration['jwt'] = {
+  cookieName: 'refreshToken',
+  expiresInAccess: 10,
+  expiresInRefresh: 10,
+  jwtSecret: 'secret',
+  redirectUrl: '/success',
+};
+
+const configMock = {
+  get: (): Configuration['jwt'] => config,
+};

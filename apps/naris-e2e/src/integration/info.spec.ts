68d17db981de2fa217c10381e9875cc81e0a3675
Rustam <tabarzin@gmail.com>
2023-06-10 14:21:58 +0300

    test: add check if a element is disabled

--- apps/naris-e2e/src/integration/info.spec.ts
+++ apps/naris-e2e/src/integration/info.spec.ts
@@ -1,7 +1,7 @@
 describe('naris overview', () => {
   it('should display info page', () => {
     cy.visit('/#!/pages/overview/info');
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    //    cy.contains('sample');
+    cy.login('user', 'user');
+    cy.get('a[title="Информация"]').should('have.attr', 'disabled', 'disabled');
   });
 });

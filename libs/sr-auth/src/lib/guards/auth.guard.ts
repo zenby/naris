8efdabdb9340a9c2f2624a84f144713871acc861
alexwaiten <xareyli@gmail.com>
2023-06-11 14:16:11 +0300

    fix: fix authentication bug

--- libs/sr-auth/src/lib/guards/auth.guard.ts
+++ libs/sr-auth/src/lib/guards/auth.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, UrlTree, Router } from '@angular/router';
+import { tap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -9,6 +10,21 @@ export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {}
 
   canActivate(): boolean | UrlTree {
-    return !!this.auth.token || this.router.createUrlTree(['/']);
+    if (this.auth.isAuth) {
+      return true;
+    } else {
+      // Попытка обновить токен
+      this.auth
+        .renewTokenV2()
+        .pipe(
+          tap(() => {
+            this.router.navigate(['pages']);
+          })
+        )
+        .subscribe();
+
+      this.router.navigate(['login']);
+      return false;
+    }
   }
 }

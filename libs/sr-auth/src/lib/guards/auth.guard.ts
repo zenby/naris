182cd324c33733dcf23d4f06352e65d8cf3c3680
alexwaiten <xareyli@gmail.com>
2023-06-12 22:06:31 +0300

    refactor: update return type of auth guard

--- libs/sr-auth/src/lib/guards/auth.guard.ts
+++ libs/sr-auth/src/lib/guards/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, UrlTree, Router } from '@angular/router';
-import { tap } from 'rxjs';
+import { switchMap, Observable, of, catchError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -9,22 +9,24 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {}
 
-  canActivate(): boolean | UrlTree {
+  canActivate(): boolean | Observable<boolean | UrlTree> {
     if (this.auth.isAuth) {
       return true;
     } else {
       // Попытка обновить токен
-      this.auth
-        .renewTokenV2()
-        .pipe(
-          tap(() => {
-            const urlTree = this.router.createUrlTree(['pages']);
-            this.router.navigateByUrl(urlTree);
-          })
-        )
-        .subscribe();
-
-      return this.router.createUrlTree(['/', 'login']);
+      return this.auth.renewTokenV2().pipe(
+        switchMap((result) => {
+          if (result.items[0].accessToken) {
+            return of(true);
+          } else {
+            return of(this.router.createUrlTree(['login']));
+          }
+        }),
+        catchError(() => {
+          // 401 и 403 нужно отлавливать вручную
+          return of(this.router.createUrlTree(['login']));
+        })
+      );
     }
   }
 }

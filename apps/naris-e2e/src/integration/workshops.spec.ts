84c964b0751ab1d9dbc8ce8783fd2ffcca8c6d39
zerodoubler <zerodoubler@gmail.com>
2023-06-14 16:09:50 +0400

    test: add test for back button to cypress spec for workshops page

--- apps/naris-e2e/src/integration/workshops.spec.ts
+++ apps/naris-e2e/src/integration/workshops.spec.ts
@@ -6,10 +6,31 @@ describe('module workshops', () => {
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(1000);
   });
+
   it('should display workshop folders list', () => {
     cy.visit('#!/pages/workshops');
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(1000);
     cy.get('soer-streams');
+  });
+
+  it('should display at least one workshop folder', () => {
+    cy.visit('#!/pages/workshops');
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000);
+    cy.get('nz-card');
+  });
+
+  it('should display back button on every workshops folder page', () => {
+    cy.visit('#!/pages/workshops');
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000);
+    cy.get('nz-card').each(($card, index, $cards) => {
+      cy.wrap($card).click();
+      cy.get('nz-card').first().as('btnBack');
+      cy.get('@btnBack').should('have.descendants', 'svg[data-icon="arrow-left"]');
+      cy.get('@btnBack').click();
+      cy.url().should('contain', '#!/pages/workshops');
+    });
   });
 });

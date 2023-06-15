ca0f4c2cb228be329c8279d3673fd9f7548aa830
Guryanov-Maksim <guryanov-mv@yandex.ru>
2023-06-15 13:16:43 +0300

    feat: add question repo

--- apps/naris-api/src/app/modules/questions/questions.module.ts
+++ apps/naris-api/src/app/modules/questions/questions.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuestionsController } from './questions.controller';
 import { JwtStrategy } from '../../common/strategies/jwt.strategy';
 import { QuestionsService } from './questions.service';
+import { QuestionEntity } from './question.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([QuestionEntity])],
   controllers: [QuestionsController],
   providers: [JwtStrategy, QuestionsService],
 })
--- apps/naris-api/src/app/modules/questions/questions.service.ts
+++ apps/naris-api/src/app/modules/questions/questions.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 import { JwtPayload } from '../../common/types/jwt-payload.interface';
+import { QuestionEntity } from './question.entity';
 
 @Injectable()
 export class QuestionsService {
+  constructor(@InjectRepository(QuestionEntity) private readonly questionRepository: Repository<QuestionEntity>) {}
+
   async create({ question }: { question: string }, executor: JwtPayload) {
     throw new Error('Not implemented');
   }

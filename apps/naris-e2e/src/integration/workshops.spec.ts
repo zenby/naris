e5af97f7ac5c58170df05a693d84b7becd6cce66
zerodoubler <zerodoubler@gmail.com>
2023-06-15 21:08:54 +0400

    test: add test for access restricted massage for workshops page

--- apps/naris-e2e/src/integration/workshops.spec.ts
+++ apps/naris-e2e/src/integration/workshops.spec.ts
@@ -7,30 +7,64 @@ describe('module workshops', () => {
     cy.wait(1000);
   });
 
-  it('should display workshop folders list', () => {
+  it('displays workshop folders list', () => {
     cy.visit('#!/pages/workshops');
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(1000);
     cy.get('soer-streams');
   });
 
-  it('should display at least one workshop folder', () => {
+  it('displays at least one workshop folder', () => {
     cy.visit('#!/pages/workshops');
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(1000);
     cy.get('nz-card');
   });
 
-  it('should display back button on every workshops folder page', () => {
+  it('displays back button on every workshops folder page', () => {
     cy.visit('#!/pages/workshops');
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(1000);
     cy.get('nz-card').each(($card, index, $cards) => {
       cy.wrap($card).click();
-      cy.get('nz-card').first().as('btnBack');
-      cy.get('@btnBack').should('have.descendants', 'svg[data-icon="arrow-left"]');
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(1000);
+      cy.get('nz-card').first().as('btnBack'); // first card is back to workshops button
+      cy.get('@btnBack').should('have.descendants', 'svg[data-icon="arrow-left"]'); // check if icon goback is present
       cy.get('@btnBack').click();
-      cy.url().should('contain', '#!/pages/workshops');
+      cy.url().should('contain', '#!/pages/workshops'); // now we are at the page where we started
+    });
+  });
+
+  it('has workshops inside every workshops folder', () => {
+    cy.visit('#!/pages/workshops');
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000);
+    cy.get('nz-card').each(($card, index, $cards) => {
+      cy.wrap($card).click();
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(1000);
+      cy.get('nz-card').should('have.length.least', 2); // first card is back button, others are supposed to be workshops
+    });
+  });
+
+  it('shows access restricted message for guest account when accessing workshop ', () => {
+    cy.visit('#!/pages/workshops');
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000);
+    cy.get('nz-card').each(($card, index, $cards) => {
+      cy.wrap($card).click();
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(1000);
+      cy.get('nz-card').each(($card1, index, $cards1) => {
+        if (index > 0) {
+          // FIXME filter elements using more functional approach
+          cy.wrap($card1).click();
+          // eslint-disable-next-line cypress/no-unnecessary-waiting
+          cy.wait(1000);
+          cy.get('nz-result').should('be.visible').contains('Данный контент доступен только на платных подписках');
+        }
+      });
     });
   });
 });

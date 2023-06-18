38c271cbb3ba36defd74c79ed040aa41919c8f20
g1tPush <109822566+g1tPush@users.noreply.github.com>
2023-06-18 21:50:33 +0300

    refactor: обновлен файл с константами

--- apps/naris-e2e/src/support/pathConstants.ts
+++ apps/naris-e2e/src/support/pathConstants.ts
@@ -1,2 +1,3 @@
 export const createNewArticlePath = '#!/pages/workbook/articles/create/new';
 export const editArticlePath = '#!/pages/workbook/articles/edit';
+export const allArticlesPath = '#!/pages/workbook/articles';

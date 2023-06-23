51b14d260c913ae8bf32113d95b5af366f100b02
Guryanov-Maksim <guryanov-mv@yandex.ru>
2023-06-23 16:29:16 +0300

    refactor: remove unused code

--- apps/naris-api/src/app/modules/questions/constants/messages.ts
+++ apps/naris-api/src/app/modules/questions/constants/messages.ts
@@ -1,11 +1,5 @@
 export default {
-  questionCreated: 'The question has been created',
-  emptyQuestion: 'The question cannot be empty',
-  questionDeleted: 'The question has been deleted',
-  questionNotFound: 'The question to delete was not found',
   questionsGettingError: 'Something went wrong. Please, reload the page.',
-  unauthorisizedQuestionCreation:
-    'The question has not been saved. A userId must be the same as the current authorized user id',
   questionCreationError: 'The question has not been saved. Please, try again later',
   questionDeleteError: 'The question has not been deleted. Please, try again later',
   questionGettingForbiddenError:

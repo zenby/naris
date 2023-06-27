c22d2272b9d22b6fd1efcba53a4a3e3de30a6a92
dizinnes <dizinnes228@gmail.com>
2023-06-27 11:09:01 +0600

    refactor(apps/naris): update create-version.js script

--- apps/naris/create-version.js
+++ apps/naris/create-version.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { exec } = require('child_process');
 
-exec('git describe --tags', (error, stdout, stderr) => {
+exec('git describe --tags --abbrev=0', (error, stdout, stderr) => {
   if (error) {
     console.log(`error: ${error.message}`);
     return;
@@ -14,8 +14,12 @@ exec('git describe --tags', (error, stdout, stderr) => {
 
   let gitTag = stdout.trim().split('-')[1];
 
-  fs.writeFile('./apps/naris/src/environments/version.ts', `export const narisVersion = '${gitTag}'`, function (err) {
-    if (err) throw err;
-    console.log('Version file is created successfully');
-  });
+  fs.writeFile(
+    './apps/naris/src/environments/version.ts',
+    `export const narisVersion = "${gitTag}";\n`,
+    function (err) {
+      if (err) throw err;
+      console.log('Version file is created successfully');
+    }
+  );
 });

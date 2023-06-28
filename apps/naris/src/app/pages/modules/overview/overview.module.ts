be37cf870c8b05e6f72637c25c8e0ec73bf662f5
mdlufy <german.panov.2018@mail.ru>
2023-06-29 00:22:27 +0300

    refactor(naris/overview): update activity module name & split modules

--- apps/naris/src/app/pages/modules/overview/overview.module.ts
+++ apps/naris/src/app/pages/modules/overview/overview.module.ts
@@ -11,14 +11,11 @@ import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { NzStatisticModule } from 'ng-zorro-antd/statistic';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
-import { ActivityCalendarModule, NoContentModule, TileModule } from '@soer/soer-components';
-import { ThumbnailCardModule } from '@soer/soer-components';
+import { ActivityModule, NoContentModule, TileModule, ThumbnailCardModule } from '@soer/soer-components';
 import { ByRoutePathResolver } from '../../../api/by-route-path.resolver';
 import { StreamService } from '../../../api/streams/stream.service';
 import { WorkshopsService } from '../../../api/workshops/workshops.service';
 import { VideoService } from '../../../services/video/video.service';
-import { NzCalendarModule } from 'ng-zorro-antd/calendar';
-
 import { MetricsComponent } from './metrics/metrics.component';
 import { InfoComponent } from './info/info.component';
 import { TargetsListComponent } from './metrics/targets-list/targets-list.component';
@@ -28,6 +25,20 @@ import { CountStatusStrategyPipe } from './metrics/metrics-list/strategies/count
 import { LatestComponent } from './latest/latest.component';
 import { CalendarPageComponent } from './calendar-page/calendar-page.component';
 import { CalendarDataService } from './calendar-page/calendar-page.service';
+
+const SOER_COMPONENTS_MODULES = [ActivityModule, NoContentModule, TileModule, ThumbnailCardModule];
+
+const NG_ZORRO_MODULES = [
+  NzSpinModule,
+  NzCardModule,
+  NzIconModule,
+  NzLayoutModule,
+  NzDividerModule,
+  NzStatisticModule,
+  NzGridModule,
+  NzTypographyModule,
+  NzFormModule,
+];
 
 @NgModule({
   declarations: [
@@ -40,25 +51,7 @@ import { CalendarDataService } from './calendar-page/calendar-page.service';
     LatestComponent,
     CalendarPageComponent,
   ],
-  imports: [
-    CommonModule,
-    ThumbnailCardModule,
-    TileModule,
-    FormsModule,
-    NzFormModule,
-    RouterModule,
-    NzSpinModule,
-    NzCardModule,
-    NzIconModule,
-    NzLayoutModule,
-    NzDividerModule,
-    NzStatisticModule,
-    NzGridModule,
-    NzTypographyModule,
-    NoContentModule,
-    NzCalendarModule,
-    ActivityCalendarModule,
-  ],
+  imports: [CommonModule, FormsModule, RouterModule, ...NG_ZORRO_MODULES, ...SOER_COMPONENTS_MODULES],
   providers: [StreamService, WorkshopsService, ByRoutePathResolver, VideoService, CalendarDataService],
   exports: [],
 })

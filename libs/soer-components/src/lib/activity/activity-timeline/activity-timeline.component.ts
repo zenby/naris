6454ff4da451a70740961d3ee0e210585612d487
mdlufy <german.panov.2018@mail.ru>
2023-06-30 02:55:49 +0300

    feat(libs/soer-components): add activity-timeline component and stories

--- libs/soer-components/src/lib/activity/activity-timeline/activity-timeline.component.ts
+++ libs/soer-components/src/lib/activity/activity-timeline/activity-timeline.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivityEventModel } from '../activity-event-model';
 
 @Component({
   selector: 'soer-activity-timeline',
   templateUrl: './activity-timeline.component.html',
   styleUrls: ['./activity-timeline.component.scss'],
 })
-export class ActivityTimelineComponent {}
+export class ActivityTimelineComponent {
+  @Input() activitiesEvents: ActivityEventModel[] = [];
+}

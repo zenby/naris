54fe55bafb1e77f3be06f7bc6a0ecbfccfc5bf3a
mdlufy <german.panov.2018@mail.ru>
2023-06-30 02:54:43 +0300

    refactor(libs/soer-components): remove unused modules

--- libs/soer-components/src/lib/activity/activity-calendar/activity-calendar.component.stories.ts
+++ libs/soer-components/src/lib/activity/activity-calendar/activity-calendar.component.stories.ts
@@ -1,13 +1,10 @@
-import { CommonModule, registerLocaleData } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
 import { importProvidersFrom } from '@angular/core';
 import ru from '@angular/common/locales/ru';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { moduleMetadata, Meta, StoryFn, applicationConfig } from '@storybook/angular';
 import { NZ_I18N, ru_RU } from 'ng-zorro-antd/i18n';
-import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-
 import { ActivityCalendarComponent } from './activity-calendar.component';
-import { NzCalendarModule } from 'ng-zorro-antd/calendar';
 import { ActivityEventModel } from '../activity-event-model';
 import { ActivityModule } from '../activity.module';
 
@@ -22,7 +19,7 @@ export default {
   component: ActivityCalendarComponent,
   decorators: [
     moduleMetadata({
-      imports: [CommonModule, ActivityModule, NzCalendarModule, NzDropDownModule, BrowserAnimationsModule],
+      imports: [ActivityModule, BrowserAnimationsModule],
       providers: [{ provide: NZ_I18N, useValue: ru_RU }],
     }),
   ],
@@ -30,7 +27,7 @@ export default {
 
 const videos: ActivityEventModel[] = Array(20)
   .fill(1)
-  .map((item, i) => {
+  .map((_, i) => {
     return {
       title: `Просмотрено видео #${i + 1}`,
       date: new Date(

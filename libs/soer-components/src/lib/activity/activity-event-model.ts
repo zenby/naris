8b4086e4c4c0efe2997fec086546a9b43c6750bd
mdlufy <german.panov.2018@mail.ru>
2023-06-30 01:53:57 +0300

    feat(libs/soer-components): add jsdoc annotation for activity model

--- libs/soer-components/src/lib/activity/activity-event-model.ts
+++ libs/soer-components/src/lib/activity/activity-event-model.ts
@@ -1,11 +1,13 @@
 export interface ActivityEventModel {
   /**
    * Название события
+   * @property {string}
    */
   title: string;
 
   /**
    * Дата события в формате ISO
+   * @property {string}
    */
   date: string;
 }

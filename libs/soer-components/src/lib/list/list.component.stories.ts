b4cb4ef073db5f4ca02eee41546f13b0c5a644db
egor_wglzfg <actzch@gmail.com>
2023-06-30 07:11:06 +0300

    refactor: refactor list story

--- libs/soer-components/src/lib/list/list.component.stories.ts
+++ libs/soer-components/src/lib/list/list.component.stories.ts
@@ -1,38 +1,79 @@
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list.component';
-import { Meta, StoryFn, moduleMetadata } from '@storybook/angular';
+import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
 import { ListModule } from './list.module';
-import { ListItem } from './list.model';
 
-export default {
-  title: 'ListComponent',
+const meta: Meta<typeof ListComponent> = {
+  title: 'Libs/Components/ListComponent',
   component: ListComponent,
   decorators: [
     moduleMetadata({
       imports: [CommonModule, ListModule],
     }),
   ],
-} as Meta<ListComponent>;
+};
 
-const items: ListItem[] = Array(5)
-  .fill(1)
-  .map((item, i) => {
-    return {
-      id: i.toString(),
-      fields: [`field ${i + 1}.${i + 1}`, `field ${i + 1}.${i + 2}`, `field ${i + 1}.${i + 3}`],
-      actions: [
-        {
-          title: 'Удалить',
-          handler: (id?: string) => {
-            alert(id);
-          },
-        },
-      ],
-    };
-  });
+export default meta;
+type Story = StoryObj<typeof ListComponent>;
 
-export const Primary: StoryFn = () => ({
-  props: {
-    items,
-  },
-});
+export const Primary: Story = {
+  args: {
+    items: Array(5)
+      .fill(1)
+      .map((item, i) => {
+        return {
+          id: i.toString(),
+          fields: [`field ${i + 1}.1`, `field ${i + 1}.2`, `field ${i + 1}.3`],
+          actions: [
+            {
+              title: 'Удалить',
+              handler: (id?: string) => {
+                alert(id);
+              },
+            },
+          ],
+        };
+      }),
+  } as unknown as Partial<typeof ListComponent>,
+};
+
+export const WithoutActions: Story = {
+  args: {
+    items: Array(6)
+      .fill(1)
+      .map((item, i) => {
+        return {
+          id: i.toString(),
+          fields: [`field ${i + 1}.1`, `field ${i + 1}.2`, `field ${i + 1}.3`],
+          actions: [],
+        };
+      }),
+  } as unknown as Partial<typeof ListComponent>,
+};
+
+export const WithDeleteAndEditActions: Story = {
+  args: {
+    items: Array(4)
+      .fill(1)
+      .map((item, i) => {
+        return {
+          id: i.toString(),
+          fields: [`field ${i + 1}.1`, `field ${i + 1}.2`, `field ${i + 1}.3`],
+          actions: [
+            {
+              title: 'Удалить',
+              handler: (id?: string) => {
+                alert(id);
+              },
+            },
+            {
+              title: 'Редактировать',
+              handler: (id?: string) => {
+                alert(id);
+              },
+            },
+          ],
+        };
+      }),
+  } as unknown as Partial<typeof ListComponent>,
+};

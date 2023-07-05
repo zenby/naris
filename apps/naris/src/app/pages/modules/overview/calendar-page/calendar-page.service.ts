d58ddcc6b3c9ff9745a89ec6675e841e15283d33
mdlufy <german.panov.2018@mail.ru>
2023-07-06 00:33:47 +0300

    refactor(naris): fix merge conflicts

--- apps/naris/src/app/pages/modules/overview/calendar-page/calendar-page.service.ts
+++ apps/naris/src/app/pages/modules/overview/calendar-page/calendar-page.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { VideoService } from '../../../../services/video/video.service';
 import { Observable, map } from 'rxjs';
-import { WathcedVideo } from '../../../../services/video/video.service';
 import { CalendarPageHelper } from './calendar-page.helper';
 import { ActivityEventModel } from '@soer/soer-components';
+import { WatchedVideo } from '../../../../services/video/video.models';
 
 @Injectable()
 export class CalendarDataService {
@@ -11,7 +11,7 @@ export class CalendarDataService {
 
   public getActivities$(): Observable<ActivityEventModel[]> {
     return this.videoService
-      .getWathcedVideos()
-      .pipe(map((videos: WathcedVideo[]) => CalendarPageHelper.mapWatchedVideosToActivities(videos)));
+      .getWatchedVideos()
+      .pipe(map((videos: WatchedVideo[]) => CalendarPageHelper.mapWatchedVideosToActivities(videos)));
   }
 }

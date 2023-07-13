03cbf1482d595868cc60989a72482c0263bc41fb
S0ER <soersoft@gmail.com>
2023-07-13 16:49:39 +0300

    chore: change jest config to nest

--- libs/sr-auth-nest/jest.config.ts
+++ libs/sr-auth-nest/jest.config.ts
@@ -1,22 +1,16 @@
-module.exports = {
+export default {
   displayName: 'sr-auth-nest',
   preset: '../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {},
-  coverageDirectory: '../../coverage/libs/sr-auth-nest',
+  testEnvironment: 'node',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
       {
         tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
       },
     ],
   },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
-  ],
+  moduleNameMapper: {},
+  moduleFileExtensions: ['ts', 'js', 'html'],
 };

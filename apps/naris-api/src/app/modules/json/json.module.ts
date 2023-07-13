a38131d23099a40b20f069c643c7b08bdd87a7ae
S0ER <soersoft@gmail.com>
2023-07-13 16:44:19 +0300

    chore: extract Manifest options to config

--- apps/naris-api/src/app/modules/json/json.module.ts
+++ apps/naris-api/src/app/modules/json/json.module.ts
@@ -5,11 +5,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { JsonEntity } from './json.entity';
 import { JwtStrategy } from '../../common/strategies/jwt.strategy';
 import { ManifestModule } from '@soer/sr-auth-nest';
+import { ManifestOptions } from '../../config/manifest.const';
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([JsonEntity]),
-    ManifestModule.forRoot({ apiUrl: 'https://stage.s0er.ru/api/user/manifest' }),
-  ],
+  imports: [TypeOrmModule.forFeature([JsonEntity]), ManifestModule.forRoot(ManifestOptions)],
   controllers: [JsonController],
   providers: [JsonService, JwtStrategy],
 })

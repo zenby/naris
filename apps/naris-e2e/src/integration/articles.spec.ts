ea17a5f9b13684d3967696592e495938d70ea9e5
qvve1rty <colsid01@mail.ru>
2023-07-21 03:45:20 +0000

    refactor: articles spec Co-authored-by: qvve1rty <colsid01@mail.ru> Co-committed-by: qvve1rty <colsid01@mail.ru>

--- apps/naris-e2e/src/integration/articles.spec.ts
+++ apps/naris-e2e/src/integration/articles.spec.ts
@@ -4,14 +4,10 @@ import { createNewArticlePath, editArticlePath, allArticlesPath, viewArticlePath
 describe('naris articles', () => {
   beforeEach(() => {
     cy.intercept('GET', '**/*.svg').as('signIn');
-    cy.intercept('GET', '**/api/v2/json/article/personal').as('personalArticles');
-    cy.intercept('GET', '**/api/v2/json/article/**').as('editArticle');
-    cy.intercept('DELETE', '/api/v2/json/article/**').as('deleteRequest');
 
     cy.login('user', 'user');
     cy.visit(`/${allArticlesPath}`);
-    cy.wait('@personalArticles');
-    cy.get('a[title="Статьи"]').should('be.visible');
+    cy.get('[data-cy="Статьи"]').should('be.visible');
     cy.removeAllExistingArticles();
   });
 
@@ -19,48 +15,45 @@ describe('naris articles', () => {
     cy.visit(`/${createNewArticlePath}`);
     cy.location('href').should('eq', Cypress.config().baseUrl + createNewArticlePath);
 
-    cy.get('.edit').should('be.visible');
-    cy.get('input[placeholder="Тема"]').type(articleTitle, { force: true });
-    cy.get('textarea').first().type(articleTextWithMd, { force: true });
+    cy.get('[data-cy="edit-block"]').should('be.visible');
+    cy.get('[data-cy="abstracte-form-input"]').type(articleTitle, { force: true });
+    cy.get('[data-cy="block-editor"]').click().first().type(articleTextWithMd, { force: true });
 
-    cy.get('.anticon-appstore-add').click();
+    cy.get('[data-cy="appstore-add"]').click();
 
     cy.get('soer-block-editor').should('have.length', 2);
-    cy.get('.anticon-up').click();
+    cy.get('[data-cy="up"]').click();
 
-    cy.get('textarea').eq(1).should('have.value', articleTextWithMd);
+    cy.get('[data-cy="block-editor"]').eq(1).should('have.value', articleTextWithMd);
 
-    cy.get('textarea').first().click();
-    cy.get('.anticon-down').click();
+    cy.get('[data-cy="block-editor"]').first().click();
+    cy.get('[data-cy="down"]').click();
 
-    cy.get('textarea').first().should('have.value', articleTextWithMd);
-    cy.get('.anticon-edit').click();
+    cy.get('[data-cy="block-editor"]').first().should('have.value', articleTextWithMd);
+    cy.get('[data-cy="edit"]').click();
 
-    cy.get('textarea').eq(1).should('not.to.have.focus');
+    cy.get('[data-cy="block-editor"]').eq(1).should('not.to.have.focus');
   });
 
   it('should check keyboard interaction on create article page', () => {
     cy.visit(`/${createNewArticlePath}`);
     cy.location('href').should('eq', Cypress.config().baseUrl + createNewArticlePath);
 
-    cy.get('.edit').should('be.visible');
+    cy.get('[data-cy="edit-block"]').should('be.visible');
 
-    cy.get('textarea').first().type(articleTextWithMd, { force: true });
+    cy.get('[data-cy="block-editor"]').click().first().type(articleTextWithMd, { force: true });
 
-    cy.get('textarea').type('{alt}{enter}');
+    cy.get('[data-cy="block-editor"]').type('{alt}{enter}');
     cy.get('soer-block-editor').should('have.length', 2);
 
-    cy.get('textarea').eq(1).type('{alt}{uparrow}');
-    cy.get('textarea').eq(1).should('have.value', `${articleTextWithMd}\n`);
+    cy.get('[data-cy="block-editor"]').eq(1).type('{alt}{uparrow}');
+    cy.get('[data-cy="block-editor"]').eq(1).should('have.value', articleTextWithMd);
 
-    cy.get('textarea').eq(1).type('{alt}{uparrow}');
-    cy.get('textarea').first().should('have.value', `${articleTextWithMd}\n`);
+    cy.get('[data-cy="block-editor"]').eq(1).type('{alt}{uparrow}');
+    cy.get('[data-cy="block-editor"]').first().should('have.value', articleTextWithMd);
 
-    cy.get('textarea').first().type('{esc}');
-    cy.get('textarea').first().should('not.to.have.focus');
-
-    cy.get('soer-mobile-menu .ng-star-inserted').first().click();
-    cy.get('soer-mobile-menu textarea').should('not.exist');
+    cy.get('[data-cy="block-editor"]').first().type('{esc}');
+    cy.get('[data-cy="block-editor"]').first().should('not.to.have.focus');
   });
 
   it('should check article creation', () => {
@@ -71,27 +64,25 @@ describe('naris articles', () => {
   it('should check view mode from main page', () => {
     cy.createArticle();
 
-    cy.get('.anticon-eye').should('be.visible').click({ force: true });
+    cy.get('[data-cy="eye-article"]').should('be.visible').click({ force: true });
     cy.location('href').should('contain', Cypress.config().baseUrl + viewArticlePath);
-    cy.get('.workbooks').should('not.exist');
+    cy.get('[data-cy="workbooks"]').should('not.exist');
   });
 
   it('should edit existing article', () => {
     cy.createArticle();
 
-    cy.get('.anticon-edit').should('be.visible').click({ force: true });
-    cy.wait('@editArticle', { timeout: 10000 });
+    cy.get('[data-cy="edit-article"]').should('be.visible').click({ force: true });
 
-    cy.get('input[placeholder="Тема"]').should('not.be.disabled');
-    cy.get('input[placeholder="Тема"]')
+    cy.get('[data-cy="abstracte-form-input"]').should('not.be.disabled');
+    cy.get('[data-cy="abstracte-form-input"]')
       .click()
       .clear()
       .type(modifiedArticleTitle)
       .should('have.value', modifiedArticleTitle);
 
-    cy.get('.anticon-save').click();
+    cy.get('[data-cy="save"]').click();
     cy.visit(`/${allArticlesPath}`);
-    cy.wait('@personalArticles');
 
     cy.contains(modifiedArticleTitle).should('exist');
   });
@@ -100,19 +91,19 @@ describe('naris articles', () => {
     cy.on('window:confirm', () => false);
     cy.createArticle();
 
-    cy.get('.anticon-edit').should('be.visible').click({ force: true });
+    cy.get('[data-cy="edit-article"]').should('be.visible').click({ force: true });
     cy.location('href').should('contain', Cypress.config().baseUrl + editArticlePath);
-    cy.get('input[placeholder="Тема"]').clear();
-    cy.get('input[placeholder="Тема"]').type(modifiedArticleTitle);
+    cy.get('[data-cy="abstracte-form-input"]').clear();
+    cy.get('[data-cy="abstracte-form-input"]').type(modifiedArticleTitle);
 
-    cy.get('.anticon-rollback').click();
-    cy.get('.anticon-rollback').should('exist');
+    cy.get('[data-cy="rollback"]').click();
+    cy.get('[data-cy="rollback"]').should('exist');
   });
 
   it('should check article delete', () => {
     cy.createArticle();
     cy.removeAllExistingArticles();
 
-    cy.contains('Начать').should('exist');
+    cy.get('[data-cy="create-workbook-button"]').should('exist');
   });
 });

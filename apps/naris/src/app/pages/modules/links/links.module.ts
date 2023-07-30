3c22e7b9ddbf269e97e3c858cc0751bd2c6488d8
S0ER <soersoft@gmail.com>
2023-07-30 21:23:43 +0300

    feat: add errors to access denied component

--- apps/naris/src/app/pages/modules/links/links.module.ts
+++ apps/naris/src/app/pages/modules/links/links.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { VideoPlayerModule } from '@soer/soer-components';
+import { AccessDeniedModule, VideoPlayerModule } from '@soer/soer-components';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { VideoViewPageComponent } from './video-view-page/video-view-page.component';
 
 @NgModule({
-  imports: [CommonModule, VideoPlayerModule, NzSpinModule],
+  imports: [CommonModule, VideoPlayerModule, NzSpinModule, AccessDeniedModule],
   declarations: [VideoViewPageComponent],
   exports: [VideoViewPageComponent],
 })
--- libs/soer-components/src/lib/access-denied/access-denied.component.ts
+++ libs/soer-components/src/lib/access-denied/access-denied.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'soer-access-denied',
   templateUrl: './access-denied.component.html',
   styleUrls: ['./access-denied.component.scss'],
 })
-export class AccessDeniedComponent {}
+export class AccessDeniedComponent {
+  @Input() errors: string[] = [];
+}

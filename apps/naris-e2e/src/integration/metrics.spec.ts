40539b466d5d42cb659d5d58aa617f6efc11d8dc
jsapro <77.3.77@mail.ru>
2023-07-31 15:03:11 +0300

    refactor: combine all tests

--- apps/naris-e2e/src/integration/metrics.spec.ts
+++ apps/naris-e2e/src/integration/metrics.spec.ts
@@ -4,14 +4,11 @@ describe('naris metrics', () => {
     cy.login('user', 'user');
   });
 
-  it('should successfully navigate to metrics page and click on every metric tile', () => {
+  it('should successfully navigate to metrics page and click on all tiles one by one', () => {
     cy.visit('/#!/pages/overview/info');
-
-    // Tab button
     cy.get('[data-cy="Метрики"]').should('not.have.attr', 'disabled');
     cy.get('[data-cy="Метрики"]').click();
     cy.get('[data-cy="Метрики"]').should('have.attr', 'disabled');
-
     cy.location().should((location) => expect(location.href).to.include('/pages/overview/metrics'));
     cy.get('[data-cy="targets/list"]').contains('Цели');
     cy.get('[data-cy="workbook"]').contains('Конспекты');
@@ -20,11 +17,6 @@ describe('naris metrics', () => {
     cy.get('[data-cy="workshops"]').contains('Воркшопы');
     cy.get('[data-cy="book"]').contains('Книга');
     cy.get('[data-cy="sources"]').contains('Исходники');
-  });
-
-  it('should display achievement tile and navigetes to General templates by clicking on it', () => {
-    cy.visit('/#!/pages/overview/metrics');
-
     cy.get('[data-cy="emptyTarget"]').click();
     cy.location().should((location) => expect(location.href).to.include('/pages/targets/templates/public'));
   });

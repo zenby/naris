2ad0fe78e9fa715a40c1d5544c96333e939f4cde
S0ER <soersoft@gmail.com>
2023-08-02 11:36:04 +0300

    test(naris-e2e): fix errors in qa test

--- apps/naris-e2e/src/integration/qa.spec.ts
+++ apps/naris-e2e/src/integration/qa.spec.ts
@@ -12,24 +12,24 @@ describe('modules questions', () => {
 
   it('should open the new question creation form, show error if saving very long question, handle it and save normal question', () => {
     cy.visit('/#!/pages/qa/my');
-    cy.get('[data-cy="plusBtn"]').click();
+    cy.get('[data-cy="plus"]').click();
     cy.url().should('equal', Cypress.config().baseUrl + '#!/pages/qa/my/create/new');
     cy.get('[data-cy="questionFormTitle"]').contains('Задайте свой вопрос');
     cy.get('[data-cy="questionInput"]').type(veryLongQuestion, {
       delay: 0,
     });
     cy.get('.ant-notification-notice').should('not.exist');
-    cy.get('[data-cy="saveBtn"]').click();
+    cy.get('[data-cy="save"]').click();
     cy.get('.ant-notification-notice').should('exist');
-    cy.get('[data-cy="rollbackBtn"]').click();
+    cy.get('[data-cy="rollback"]').click();
     cy.url().should('equal', Cypress.config().baseUrl + '#!/pages/qa/my');
-    cy.get('[data-cy="plusBtn"]').click();
+    cy.get('[data-cy="plus"]').click();
     cy.get('[data-cy="questionInput"]').type(veryLongQuestion, {
       delay: 0,
     });
-    cy.get('[data-cy="saveBtn"]').click();
+    cy.get('[data-cy="save"]').click();
     cy.get('[data-cy="questionInput"]').clear().type(permissibleQuestion).should('have.value', permissibleQuestion);
-    cy.get('[data-cy="saveBtn"]').click();
+    cy.get('[data-cy="save"]').click();
     cy.url().should('equal', Cypress.config().baseUrl + '#!/pages/qa/my');
     cy.wait('@getUpdatedQuestions');
     cy.get('.ant-message-notice-content').should('exist');
@@ -39,37 +39,37 @@ describe('modules questions', () => {
   it('should delete selected question, then all questions and then show proposal to ask first question', () => {
     cy.visit('#!/pages/qa/my/create/new'); // add 2 new questions.
     cy.get('[data-cy="questionInput"]').type(permissibleQuestion);
-    cy.get('[data-cy="saveBtn"]').click();
+    cy.get('[data-cy="save"]').click();
     cy.wait('@getUpdatedQuestions');
-    cy.get('[data-cy="plusBtn"]').click();
+    cy.get('[data-cy="plus"]').click();
     cy.get('[data-cy="questionInput"]').type(permissibleQuestion);
-    cy.get('[data-cy="saveBtn"]').click();
+    cy.get('[data-cy="save"]').click();
     cy.wait('@getUpdatedQuestions');
     cy.url().should('equal', Cypress.config().baseUrl + '#!/pages/qa/my');
 
     cy.get('[data-cy="qaListItemDescription"]').then((qaList) => {
       const listLen = Cypress.$(qaList).length - 1;
 
-      cy.get('[data-cy="questionDeleteBtn"]').last().click();
+      cy.get('[data-cy="questionDelete"]').last().click();
       cy.wait('@getUpdatedQuestions');
       cy.get('[data-cy="qaListItemDescription"]').its('length').should('equal', listLen);
     });
     cy.visit('#!/pages/qa/my/create/new');
     cy.get('[data-cy="questionInput"]').type(permissibleQuestion);
-    cy.get('[data-cy="saveBtn"]').click();
+    cy.get('[data-cy="save"]').click();
     cy.wait('@getUpdatedQuestions');
-    cy.get('[data-cy="questionDeleteBtn"]').then((delBtnList) => {
+    cy.get('[data-cy="questionDelete"]').then((delBtnList) => {
       //delete all questions
       let listLen = Cypress.$(delBtnList).length;
 
       while (listLen) {
-        cy.get('[data-cy="questionDeleteBtn"]').last().click();
+        cy.get('[data-cy="questionDelete"]').last().click();
         cy.wait('@getUpdatedQuestions');
         listLen--;
       }
     });
     cy.get('[data-cy="firstAskProposal"]').contains('Задать первый вопрос');
-    cy.get('[data-cy="firstAskBtn"]').should('exist');
+    cy.get('[data-cy="firstAsk"]').should('exist');
   });
 
   it('should switch between "my questions" and "all questions" page', () => {

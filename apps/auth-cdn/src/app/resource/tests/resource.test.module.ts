9572c6bb7f4aff1699aed86b19cafedadb34ff2b
Guryanov-Maksim <guryanov-mv@yandex.ru>
2023-08-06 11:14:42 +0300

    test: add a test upload-resources module

--- apps/auth-cdn/src/app/resource/tests/resource.test.module.ts
+++ apps/auth-cdn/src/app/resource/tests/resource.test.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { ResourceController } from '../resource.controller';
 import { ResourceService } from '../resource.service';
 import { ResourceRepository } from '../resource.repository';
-import { UploadResourcesModule } from '../upload-resources.module';
+import { TestUploadResourcesModule } from './upload-resources.test.module';
 import { TestResourceRepository } from './resource.test.repository';
 
 @Module({
-  imports: [UploadResourcesModule],
+  imports: [TestUploadResourcesModule],
   controllers: [ResourceController],
   providers: [
     ResourceService,

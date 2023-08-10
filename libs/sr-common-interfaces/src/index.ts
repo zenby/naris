d169b6b893e5fedf4832d9846942ae399522404e
S0ER <soersoft@gmail.com>
2023-08-10 12:22:20 +0300

    feat: add common PageLink interface

--- libs/sr-common-interfaces/src/index.ts
+++ libs/sr-common-interfaces/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/http-json-result.interface';
 export * from './lib/common-config.interface';
 export * from './lib/user-role.interface';
 export * from './lib/user-manifest.interface';
+export * from './lib/frontend/link.interface';
---
+++ libs/sr-common-interfaces/src/lib/frontend/link.interface.ts
@@ -0,0 +1,5 @@
+export interface AppPageLink {
+  linkType: string;
+  value: string | number;
+  label: string;
+}
